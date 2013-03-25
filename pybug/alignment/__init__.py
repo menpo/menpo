1d0e160ce61c05dc1233c78b7572013c6f2ef9dd
James Booth <jabooth@gmail.com>
2013-03-25 23:14:03 +0000

    alignments are now single source to target - greatly simplifies implimentation

--- pybug/alignment/__init__.py
+++ pybug/alignment/__init__.py
@@ -1,11 +1,56 @@
 import numpy as np
 import hashlib
 
+"""
+This package contains methods for aligning a number of source objects
+to a target. Objects are provided as an iterable of ndarrays of
+shape(n_dim, n_landmarks). It is legal to provide a single source
+(which need not be contained in an iterable) so long as a single
+target is also provided. It is legal to provide no target so long
+as multiple sources are provided - in such cases, the target is
+set to the mean of the sources. All sources and targets must
+have the same n_dim, and the same n_landmarks.
+
+The resulting subclasses of alignment provide the following methods:
+  1. align(sources, source_lms)
+  returns the aligned version of the source input points when the source
+  input points are considered to be points in the space of the source_lms.
+  source_lms has to be a
+
+"""
+
+class AlignmentError(Exception):
+  pass
+
 class Alignment(object):
+  """ Aligns a single source object to a target.
+  """
+
+  def __init__(self, source, target):
+    """ sources - ndarray of landmarks which will be aligned
+                    e.g. [landmarks1, landmakrks2,...landmarksn] where
+                    landmarksi is an ndarray of dimension [n_landmarks x n_dim]
+
+        target  - a single numpy array (of the same dimension of sources) which
+                  every instance of source will be aligned to. If not present,
+                  target is set to the mean source position.
+    """
+    self.source = source
+    self.aligned_source = self.source.copy()
+    try:
+      self.n_landmarks, self.n_dim = self.source.shape
+    except ValueError:
+      raise AlignmentError('Data is being provided in an invalid format'\
+          ' - must have shape (n_landmarks, n_dim)')
+    assert self.n_dim, self.n_landmarks == target.shape
+    self.target = target
+
+
+class ParallelAlignment(object):
 
   def __init__(self, sources, target=None):
     """ sources - a list of numpy arrays of landmarks which will be aligned
-                    e.g. [landmarks1, landmakrks2,...landmarksn] where 
+                    e.g. [landmarks1, landmakrks2,...landmarksn] where
                     landmarksi is an ndarray of dimension [n_landmarks x n_dim]
       KWARGS
         target  - a single numpy array (of the same dimension of sources) which
@@ -48,6 +93,7 @@ class Alignment(object):
   @property
   def n_sources(self):
     return self.sources.shape[2]
+
 
 def _numpy_hash(array):
   """ Efficiently generates a hash of a numpy array.
--- pybug/alignment/rigid.py
+++ pybug/alignment/rigid.py
@@ -2,6 +2,88 @@ import numpy as np
 from . import Alignment, _numpy_hash
 from scipy import linalg
 
+class LinearTransformation(object):
+
+  def __init__(self, n_dim):
+    self.n_dim = n_dim
+
+
+class Rotation(LinearTransformation):
+  pass
+
+
+class Scale(LinearTransformation):
+  pass
+
+class Translation(LinearTransformation):
+  pass
+
+
+
+def h_translation_matrix(translation_vector):
+  dim = translation_vector.size
+  matrix = np.eye(dim + 1)
+  matrix[-1, :-1] = translation_vector
+  return matrix
+
+def h_scale_matrix(scale_vector, dim=None):
+  if dim == None:
+    dim = scale_vector.size
+  matrix = np.eye(dim + 1)
+  np.fill_diagonal(matrix,scale_vector)
+  # set the corner value back to 1
+  matrix[-1, -1] = 1
+  return matrix
+
+def h_rotation_matrix(rotation_matrix):
+  dim =  rotation_matrix.shape[0]
+  matrix = np.eye(dim + 1)
+  matrix[:-1, :-1] = rotation_matrix
+  return matrix
+
+
+class ParallelRigidAlignment(RigidAlignment):
+  """ Abstract class specializing in rigid alignments. As all such alignments
+      are affine, a 'h_transforms' list is present, storing a homogeneous
+      transformation matrix for each source specifying the transform it has
+      undergone to match the target.
+  """
+  def __init__(self, sources, **kwargs):
+    Alignment.__init__(self, sources, **kwargs)
+    self.h_transforms = []
+
+  @property
+  def scalerotation_matrices(self):
+    """ Returns a list of 2 dimensional numpy arrays, each of shape
+        (n_dimensions,n_dimensions) which can be applied to each source frame
+        to rigidly align to the target frame. Needs to be used in conjunction
+        with translation_vectors. (source*scalerotatation + translation -> target)
+    """
+    return [matrix[:-1,:-1] for matrix in self.h_transforms]
+
+  @property
+  def translation_vectors(self):
+    """ Returns a list of translation vectors, each of shape (n_dimensions,)
+        which can be applied to each source frame to rigidly align to the
+        target frame (source*scalerotatation + translation -> target)
+    """
+    return [matrix[-1,:-1] for matrix in self.h_transforms]
+    pass
+
+  def h_transform_for_source(self, source):
+    i = self._lookup[_numpy_hash(source)]
+    return self.h_transforms[i]
+
+  def scalerotation_translation_for_source(self, source):
+    i = self._lookup[_numpy_hash(source)]
+    return self.scalerotation_matrices[i], self.translation_vectors[i]
+
+  def _normalise_transformation_matrices(self):
+    """ Ensures that the transformation matrix has a unit z scale,
+        and is affine (e.g. bottom row = [0,0,0,1] for dim = 3)
+    """
+    pass
+
 class RigidAlignment(Alignment):
   """ Abstract class specializing in rigid alignments. As all such alignments
       are affine, a 'h_transforms' list is present, storing a homogeneous
@@ -44,6 +126,66 @@ class RigidAlignment(Alignment):
     """
     pass
 
+
+class ParallelProcrustes(ParallelRigidAlignment):
+
+  def __init__(self, sources, **kwargs):
+    RigidAlignment.__init__(self, sources, **kwargs)
+
+  def general_alignment(self):
+    # stores the items used in each procrustes step
+    self.operations = []
+    error = 999999999
+    while error > 0.0001:
+      self._procrustes_step()
+      old_target = self.target
+      self.target = self.aligned_sources.mean(axis=-1)[...,np.newaxis]
+      error = np.sum((self.target - old_target)**2)
+      print 'error is ' + `error`
+    self.h_transforms = []
+    for i in range(self.n_sources):
+      self.h_transforms.append(np.eye(self.n_dimensions+1))
+      for ops in self.operations:
+        t = h_translation_matrix(ops['translate'][..., i].flatten())
+        s = h_scale_matrix(ops['rescale'][..., i].flatten(), dim=self.n_dimensions)
+        r = h_rotation_matrix(ops['rotation'][i])
+        self.h_transforms[i] = np.dot(self.h_transforms[i],
+                               np.dot(t,
+                               np.dot(s,
+                                      r)))
+
+  def _procrustes_step(self):
+    print 'taking Procrustes step'
+    ops = {}
+    # calculate the translation required for each source to align the sources'
+    # centre of mass to the the target centre of mass
+    translation = (self.target.mean(axis=0) -
+        self.aligned_sources.mean(axis=0))[np.newaxis, ...]
+    # apply the translation to each source respectively
+    self.aligned_sources += translation
+    ops['translate'] = translation
+    # calcuate the frobenious norm of each shape as our metric
+    scale_sources = np.sqrt(np.apply_over_axes(np.sum,
+      (self.aligned_sources - self.aligned_sources.mean(axis=0))**2,
+                           [0,1]))
+    scale_target = np.sqrt(np.sum((self.target -
+                                  self.target.mean(axis=0))**2))
+    rescale = scale_target/scale_sources
+    self.aligned_sources = self.aligned_sources*rescale
+    ops['rescale'] = rescale
+    rotations = []
+    #for each source
+    for i in range(self.n_sources):
+      # calculate the correlation along each dimension
+      correlation = np.dot(self.aligned_sources[...,i].T, self.target[...,0])
+      U,D,Vt = np.linalg.svd(correlation)
+      # find the optimal rotation to minimise rotational differences
+      rotation = np.dot(U, Vt)
+      rotations.append(rotation)
+      # apply the rotation
+      self.aligned_sources[...,i] = np.dot(self.aligned_sources[...,i], rotation)
+    ops['rotation'] = rotations
+    self.operations.append(ops)
 
 class Procrustes(RigidAlignment):
 
@@ -104,25 +246,4 @@ class Procrustes(RigidAlignment):
       self.aligned_sources[...,i] = np.dot(self.aligned_sources[...,i], rotation)
     ops['rotation'] = rotations
     self.operations.append(ops)
-
-def h_translation_matrix(translation_vector):
-  dim = translation_vector.size
-  matrix = np.eye(dim + 1)
-  matrix[-1, :-1] = translation_vector
-  return matrix
-
-def h_scale_matrix(scale_vector, dim=None):
-  if dim == None:
-    dim = scale_vector.size
-  matrix = np.eye(dim + 1)
-  np.fill_diagonal(matrix,scale_vector)
-  # set the corner value back to 1
-  matrix[-1, -1] = 1
-  return matrix
-
-def h_rotation_matrix(rotation_matrix):
-  dim =  rotation_matrix.shape[0]
-  matrix = np.eye(dim + 1)
-  matrix[:-1, :-1] = rotation_matrix
-  return matrix
 
