3cd7d0e6dfc5b401e7deba0cfb3ae71ddfe7bdf9
James Booth <jabooth@gmail.com>
2013-03-28 17:51:14 +0000

    moved mesh to shape folder

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize("./pybug/geodesics/exact.pyx",))
-setup(ext_modules = cythonize("./pybug/mesh/cpptrianglemesh.pyx",))
+setup(ext_modules = cythonize("./pybug/shape/cpptrianglemesh.pyx",))
