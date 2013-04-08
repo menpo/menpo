0e3efd8d77aca0cf289d246f4c248a0456914783
James Booth <jabooth@gmail.com>
2013-04-08 17:02:44 +0100

    changes to setup requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import subprocess
-
-cython_modules = ["pybug/transform/geodesics/kirsanov.pyx",
-                  "pybug/spatialdata/mesh/cpptrianglemesh.pyx"]
-
 
 # grab the submodule and update if required
 subprocess.call("git submodule update --init -q", shell=True)
 
-
+cython_modules = ["pybug/transform/geodesics/kirsanov.pyx",
+                  "pybug/spatialdata/mesh/cpptrianglemesh.pyx"]
 
 setup(name='pybug',
-      version='0.1',
-      description='iBUG Facial Modelling Toolkit',
-      author='James Booth',
-      author_email='james.booth08@imperial.ac.uk',
-      ext_modules = cythonize(cython_modules, nthreads=2, quiet=True)
+      version = '0.1',
+      description = 'iBUG Facial Modelling Toolkit',
+      author = 'James Booth',
+      author_email = 'james.booth08@imperial.ac.uk',
+      ext_modules = cythonize(cython_modules, nthreads=2, quiet=True),
+      packages = find_packages(),
+      install_requires = ['numpy>=1.6']
       )
+
