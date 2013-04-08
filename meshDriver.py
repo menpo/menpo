d740141d9fb4f48c216d8a59632669fe0273dbd9
James Booth <jabooth@gmail.com>
2013-04-08 16:27:31 +0100

    big cleanup, my own scripts will now be kept seperate

--- meshDriver.py
+++
@@ -1,9 +0,0 @@
-from pybug.mesh import face
-import numpy as np
-
-coords = np.array([[0.,0,0],[1,0,0],[0,1,0],[1,1,0]])
-coordsIndex = np.array([[0,1,2],[3,2,1]],dtype=np.uint32)
-myMesh = face.Face(coords=coords, coordsIndex=coordsIndex)
-myMesh.verifyAttachments()
-
-
--- phi_comp.py
+++
@@ -1,9 +0,0 @@
-from pybug.importer.model import import_face 
-import numpy as np
-import matplotlib.pyplot as plt
-plt.interactive(True)
-
-sphere = import_face('~/sphere-160k.obj')
-geo = sphere.geodesics_about_vertices([0])
-plt.hist(geo['phi'], bins=1000)
-
