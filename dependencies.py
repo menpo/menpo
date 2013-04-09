cfd472394ef2d6acd2a43cb27aa39d31a35a051f
James Booth <jabooth@gmail.com>
2013-04-10 00:34:27 +0100

    fix torando typo

--- dependencies.py
+++ dependencies.py
@@ -2,7 +2,7 @@
 requirements = ['numpy>=1.7.1',
                 'Cython>=0.18',
                 'ipython>=0.13.2',
-                'torando>=3.0.1',
+                'tornado>=3.0.1',
                 'pyzmq>=13.0.2',
                 'matplotlib>=1.2.1',
                 'Sphinx>=1.2b1',
