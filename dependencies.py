34f611f79843d98d83dc109ca304737ac5d86129
James Booth <jabooth@gmail.com>
2013-04-10 00:31:39 +0100

    fleshed out dependencies

--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,11 @@
 
-requirements = ['Cython>=0.18',
-                'numpy>=1.7.1',
-                'ipython>=0.13.1',
-                'Sphinx>=1.1.3']
+requirements = ['numpy>=1.7.1',
+                'Cython>=0.18',
+                'ipython>=0.13.2',
+                'torando>=3.0.1',
+                'pyzmq>=13.0.2',
+                'matplotlib>=1.2.1',
+                'Sphinx>=1.2b1',
+                'scipy>=0.12.0',
+                'mayavi>=4.3.0']
 
