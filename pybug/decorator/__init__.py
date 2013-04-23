e06130b10e986bc016c2456a7bf6a6f6155896cc
James Booth <jabooth@gmail.com>
2013-04-23 16:53:01 +0100

    decorator docstring > 79 width fix

--- pybug/decorator/__init__.py
+++ pybug/decorator/__init__.py
@@ -6,15 +6,17 @@ def const(f, *args, **kwargs):
     """
     Decorator to maintain constness of parameters.
 
-    This should be used on any function that takes ndarrays as parameters. This helps protect
-    against potential subtle bugs caused by the pass by reference nature of python arguments. Copy
+    This should be used on any function that takes ndarrays as parameters.
+    This helps protect against potential subtle bugs caused by the pass by
+    reference nature of python arguments. Copy
     is assumed to a shallow copy.
 
     :param f: function to wrap
     :param args: function f's args
     :param kwargs: function f's kwargs
                    copy {True, False}
-                       If False then don't copy the parameters - use with caution
+                       If False then don't copy the parameters - use with
+                       caution
     """
     copy = kwargs.get('copy', True)
     if copy:
