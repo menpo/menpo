dcf3999e251ea5909f694c00b7ecc45cc0fc219a
James Booth <jabooth@gmail.com>
2013-04-27 09:05:49 +0100

    PIL->Pillow, remove numpydoc, tidy mesh import
    Remove the dependency on numpydoc as for now we will just be using
    default docstring format. Changed Image import dep from PIL to Pillow -
    the only place this is used is in the mesh importer, so the import has
    been updated. Few other little fixes in this mesh file too (god bless
    PyCharm)

--- dependencies.py
+++ dependencies.py
@@ -7,8 +7,7 @@ requirements = ['numpy>=1.7.1',
                 'matplotlib>=1.2.1',
                 'Sphinx>=1.2b1',
                 'scipy>=0.12.0',
-                'PIL>=1.1.7',
-                'numpydoc>=0.4',
+                'Pillow>=2.0.0',
                 'nose>=1.3.0',
                 'mayavi>=4.3.0']
 
