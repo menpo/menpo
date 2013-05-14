7cf43c0953346a6565d78eb863c9f8afd13b9aa1
James Booth <jabooth@gmail.com>
2013-05-14 18:05:34 +0100

    testbed setup

--- pybug/rasterize/opengl.pyx
+++ pybug/rasterize/opengl.pyx
@@ -1,5 +1,5 @@
 # distutils: language = c++
-# distutils: sources = ./pybug/rasterize/cpp/Rasterizer.cpp
+# distutils: sources = ./pybug/rasterize/matlabcpp/MM3DRenderer.cpp ./pybug/rasterize/matlabcpp/GLRFramework.cpp
 # distutils: libraries = GLU GL glut GLEW
 from libcpp.vector cimport vector
 from libc.stdint cimport uint8_t
@@ -9,19 +9,21 @@ cimport numpy as np
 
 
 # externally declare the C++ classes
-cdef extern from "./cpp/Rasterizer.h":
+cdef extern from "./matlabcpp/MM3DRenderer.h":
 
-    cdef cppclass Rasterizer:
-        Rasterizer(double* tpsCoord_in, float* coord_in, size_t numCoords_in,
+    cdef cppclass MM3DRenderer:
+        MM3DRenderer(double* tpsCoord_in, float* coord_in, size_t numCoords_in,
                      unsigned int* coordIndex_in, size_t numTriangles_in,
                      float* texCoord_in, uint8_t* textureImage_in,
-                     size_t textureWidth_in, size_t textureHeight_in)
-        void return_FB_pixels(uint8_t* pixels,
-                            float* coords, int width, int height)
+                     size_t textureWidth_in, size_t textureHeight_in,
+                     bool INTERACTIVE_MODE)
+        void returnFBPixels(int argc, char **argv, uint8_t* pixels,
+                            float* coords, int width,
+                            int height)
 
 
 cdef class OpenGLRasterizer:
-    cdef Rasterizer* thisptr
+    cdef MM3DRenderer* thisptr
     cdef unsigned t_width
     cdef unsigned t_height
     cdef unsigned n_coords
@@ -32,11 +34,12 @@ cdef class OpenGLRasterizer:
                   np.ndarray[unsigned, ndim=2, mode="c"] trilist not None,
                   np.ndarray[float, ndim=2, mode="c"] tcoords not None,
                   np.ndarray[uint8_t, ndim=3, mode="c"] texture not None):
-        self.thisptr = new Rasterizer(&tpsCoords[0,0], &coords[0,0],
+        self.thisptr = new MM3DRenderer(&tpsCoords[0,0], &coords[0,0],
                                          coords.shape[0], &trilist[0,0],
                                          trilist.shape[0],
                                          &tcoords[0,0], &texture[0,0,0],
-                                         texture.shape[0], texture.shape[1])
+                                         texture.shape[0], texture.shape[1],
+                                         False)
         self.t_width = texture.shape[0]
         self.t_height = texture.shape[1]
         self.n_coords = coords.shape[0]
@@ -47,12 +50,16 @@ cdef class OpenGLRasterizer:
 
     @property
     def pixels(self):
-        cdef np.ndarray[uint8_t, ndim=3, mode='c'] pixels = \
-            np.empty((self.t_width, self.t_height, 4), dtype=np.uint8)
+        # cdef np.ndarray[uint8_t, ndim=3, mode='c'] pixels = \
+        #     np.empty((self.t_width, self.t_height, 4), dtype=np.uint8)
+        cdef np.ndarray[uint8_t, ndim=1, mode='c'] pixels = \
+             np.empty((self.t_width * self.t_height * 4), dtype=np.uint8)
         cdef np.ndarray[float, ndim=3, mode='c'] coords = \
             np.empty((self.t_width, self.t_height, 3), dtype=np.float32)
         print self.t_width
         print self.t_height
-        self.thisptr.return_FB_pixels(&pixels[0,0,0], &coords[0,0,0],
+        cdef pystring = 'hello'.encode('UTF-8')
+        cdef char* b = pystring
+        self.thisptr.returnFBPixels(1, &b, &pixels[0], &coords[0,0,0],
                                     self.t_width, self.t_height)
         return pixels, coords
