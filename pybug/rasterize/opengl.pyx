b5fa376df75fd990b2d57ced3a8b9f4865405555
James Booth <jabooth@gmail.com>
2013-05-15 13:33:58 +0100

    rendering working in python

--- pybug/rasterize/opengl.pyx
+++ pybug/rasterize/opengl.pyx
@@ -26,40 +26,36 @@ cdef class OpenGLRasterizer:
     cdef MM3DRenderer* thisptr
     cdef unsigned t_width
     cdef unsigned t_height
-    cdef unsigned n_coords
+    cdef unsigned n_points
+    cdef unsigned n_tris
 
     def __cinit__(self,
-                  np.ndarray[double, ndim=2, mode="c"] tpsCoords not None ,
-                  np.ndarray[float, ndim=2, mode="c"] coords not None,
+                  np.ndarray[double, ndim=2, mode="c"] points not None ,
+                  np.ndarray[float, ndim=2, mode="c"] color not None,
                   np.ndarray[unsigned, ndim=2, mode="c"] trilist not None,
                   np.ndarray[float, ndim=2, mode="c"] tcoords not None,
                   np.ndarray[uint8_t, ndim=3, mode="c"] texture not None):
-        self.thisptr = new MM3DRenderer(&tpsCoords[0,0], &coords[0,0],
-                                         coords.shape[0], &trilist[0,0],
-                                         trilist.shape[0],
-                                         &tcoords[0,0], &texture[0,0,0],
-                                         texture.shape[0], texture.shape[1],
+        self.t_height = texture.shape[0]
+        self.t_width = texture.shape[1]
+        self.n_points = color.shape[0]
+        self.n_tris = trilist.shape[0]
+        self.thisptr = new MM3DRenderer(&points[0,0], &color[0,0],
+                                         self.n_points, &trilist[0,0],
+                                         self.n_tris, &tcoords[0,0],
+                                         &texture[0,0,0],
+                                         self.t_width, self.t_height,
                                          False)
-        self.t_width = texture.shape[0]
-        self.t_height = texture.shape[1]
-        self.n_coords = coords.shape[0]
 
     def __dealloc__(self):
-        print "Is this going to cause an issue?\n"
-        del self.thisptr
+       del self.thisptr
 
-    @property
-    def pixels(self):
-        # cdef np.ndarray[uint8_t, ndim=3, mode='c'] pixels = \
-        #     np.empty((self.t_width, self.t_height, 4), dtype=np.uint8)
-        cdef np.ndarray[uint8_t, ndim=1, mode='c'] pixels = \
-             np.empty((self.t_width * self.t_height * 4), dtype=np.uint8)
+    def pixels(self, render_width, render_height):
+        cdef np.ndarray[uint8_t, ndim=3, mode='c'] pixels = \
+            np.empty((render_width, render_height, 4), dtype=np.uint8)
         cdef np.ndarray[float, ndim=3, mode='c'] coords = \
-            np.empty((self.t_width, self.t_height, 3), dtype=np.float32)
-        print self.t_width
-        print self.t_height
+            np.empty((render_width, render_height, 3), dtype=np.float32)
         cdef pystring = 'hello'.encode('UTF-8')
         cdef char* b = pystring
-        self.thisptr.returnFBPixels(1, &b, &pixels[0], &coords[0,0,0],
-                                    self.t_width, self.t_height)
+        self.thisptr.returnFBPixels(1, &b, &pixels[0,0,0], &coords[0,0,0],
+                                    render_width, render_height)
         return pixels, coords
