Fix the image gradient and other changes for AAMs

pybug/align/lucaskanade/base.py
@@ -4,7 +4,7 @@ import numpy as np
 from pybug.warp.base import scipy_warp
 
 
-class LucasKanade:
+class LucasKanade(object):
     r"""
     An abstract base class for implementations of Lucas-Kanade [1]_
     type algorithms.
@@ -80,17 +80,15 @@ class LucasKanade:
     """
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, image, residual, transform,
+    def __init__(self, residual, transform,
                  warp=scipy_warp, optimisation='GN', update_step=0.001,
                  eps=1 ** -10):
         # set basic state for all Lucas Kanade algorithms
         self.initial_transform = transform
         self.TransformClass = transform.__class__
-        self.image = image
         self.residual = residual
         self.update_step = update_step
         self.eps = eps
-        self.transforms = [self.initial_transform]
 
         # select the optimisation approach and warp function
         self._calculate_delta_p = self._select_optimisation(optimisation)
@@ -123,7 +121,13 @@ class LucasKanade:
 
         return solve(H_lm, sd_delta_p)
 
-    def align(self, max_iters=30):
+    def _precompute(self):
+        """
+        Performs pre-computations related to specific alignment algorithms
+        """
+        pass
+
+    def align(self, image, params, max_iters=30):
         r"""
         Perform an alignment using the Lukas-Kanade framework.
 
@@ -139,7 +143,9 @@ class LucasKanade:
             The final transform that optimally aligns the source to the
             target.
         """
-        self.transforms = [self.initial_transform]
+        # TODO: define a consistent multi-resolution logic
+        self.transforms = [self.initial_transform.from_vector(params)]
+        self.image = image
         return self._align(max_iters)
 
     @abc.abstractmethod
@@ -185,26 +191,33 @@ class LucasKanade:
 
 class ImageLucasKanade(LucasKanade):
 
-    def __init__(self, image, template, residual, transform,
-                 warp=scipy_warp,
-                 optimisation='GN', update_step=0.001,
+    def __init__(self, template, residual, transform,
+                 warp=scipy_warp, optimisation='GN', update_step=0.001,
                  eps=10 ** -6):
         super(ImageLucasKanade, self).__init__(
-            image, residual, transform, warp, optimisation, update_step, eps)
+            residual, transform,
+            warp, optimisation, update_step, eps)
         # in image alignment, we align a template image to the target image
         self.template = template
+
+        # pre-compute
+        self._precompute()
 
 
 class AppearanceModelLucasKanade(LucasKanade):
 
-    def __init__(self, image, model, residual, transform,
+    def __init__(self, model, residual, transform,
                  warp=scipy_warp, optimisation='GN', update_step=0.001,
                  eps=10 ** -6):
         super(AppearanceModelLucasKanade, self).__init__(
-            image, residual, transform, warp, optimisation, update_step, eps)
-        # in appearance alignment, we align an appearance model to the target
-        # image
+            residual, transform,
+            warp, optimisation, update_step, eps)
+        # in appearance model alignment, we align an appearance model to the
+        # target image
         self.appearance_model = model
+
+        # pre-compute
+        self._precompute()
 
 
 class ImageForwardAdditive(ImageLucasKanade):
@@ -253,13 +266,16 @@ class ImageForwardAdditive(ImageLucasKanade):
 
 class ImageForwardCompositional(ImageLucasKanade):
 
+    def _precompute(self):
+        # Compute the Jacobian of the warp
+        self.dW_dp = self.initial_transform.jacobian(
+            self.template.mask.true_indices)
+
+        pass
+
     def _align(self, max_iters=30):
         # Initial error > eps
         error = self.eps + 1
-
-        # Compute the Jacobian of the warp
-        dW_dp = self.optimal_transform.jacobian(
-            self.template.mask.true_indices)
 
         # Forward Compositional Algorithm
         while self.n_iters < (max_iters - 1) and error > self.eps:
@@ -267,8 +283,13 @@ class ImageForwardCompositional(ImageLucasKanade):
             IWxp = self._warp(self.image, self.template,
                               self.optimal_transform)
 
+            # TODO: add "forward_compositional" kwarg with options
+            # In the forward compositional algorithm there are two different
+            # ways of computing the steepest descent images:
+            #   1. V[I(x)](W(x,p)) * dW/dx * dW/dp
+            #   2. V[I(W(x,p))] * dW/dp -> this is what is currently used
             # Compute steepest descent images, VI_dW_dp
-            VI_dW_dp = self.residual.steepest_descent_images(IWxp, dW_dp)
+            VI_dW_dp = self.residual.steepest_descent_images(IWxp, self.dW_dp)
 
             # Compute Hessian and inverse
             self._H = self.residual.calculate_hessian(VI_dW_dp)
@@ -293,19 +314,23 @@ class ImageForwardCompositional(ImageLucasKanade):
 
 class ImageInverseCompositional(ImageLucasKanade):
 
-    def _align(self, max_iters=30):
-        # Initial error > eps
-        error = self.eps + 1
-
+    def _precompute(self):
         # Compute the Jacobian of the warp
-        dW_dp = self.optimal_transform.jacobian(
+        dW_dp = self.initial_transform.jacobian(
             self.template.mask.true_indices)
 
         # Compute steepest descent images, VT_dW_dp
-        VT_dW_dp = self.residual.steepest_descent_images(self.template, dW_dp)
+        self.VT_dW_dp = self.residual.steepest_descent_images(
+            self.template, dW_dp)
 
         # Compute Hessian and inverse
-        self._H = self.residual.calculate_hessian(VT_dW_dp)
+        self._H = self.residual.calculate_hessian(self.VT_dW_dp)
+
+        pass
+
+    def _align(self, max_iters=30):
+        # Initial error > eps
+        error = self.eps + 1
 
         # Baker-Matthews, Inverse Compositional Algorithm
         while self.n_iters < (max_iters - 1) and error > self.eps:
@@ -314,8 +339,8 @@ class ImageInverseCompositional(ImageLucasKanade):
                               self.optimal_transform)
 
             # Compute steepest descent parameter updates
-            sd_delta_p = self.residual.steepest_descent_update(VT_dW_dp, IWxp,
-                                                               self.template)
+            sd_delta_p = self.residual.steepest_descent_update(
+                self.VT_dW_dp, IWxp, self.template)
 
             # Compute gradient descent parameter updates
             delta_p = np.real(self._calculate_delta_p(sd_delta_p))

pybug/image/base.py
@@ -641,18 +641,33 @@ class Image(AbstractImage):
             # Concatenate gradient list into an array (the new_image)
             new_image = np.concatenate(gradients, axis=-1)
         else:
-            masked_square_image = self.mask_bounding_pixels(boundary=3)
-            bounding_mask = self.mask.true_bounding_extent_slicer(3)
-            gradients = [np.gradient(g) for g in
-                         np.rollaxis(masked_square_image, -1)]
+            #masked_square_image = self.mask_bounding_pixels(boundary=3)
+            #bounding_mask = self.mask.true_bounding_extent_slicer(3)
+            #gradients = [np.gradient(g) for g in np.rollaxis(
+            # masked_square_image, -1)]
             # Flatten the lists
-            gradients = list(itertools.chain.from_iterable(gradients))
+            #gradients = list(itertools.chain.from_iterable(gradients))
             # Add an extra axis for broadcasting
-            gradients = [g[..., None] for g in gradients]
+            #gradients = [g[..., None] for g in gradients]
             # Concatenate gradient list into a vector
-            gradient_array = np.concatenate(gradients, axis=-1)
+            #gradient_array = np.concatenate(gradients, axis=-1)
             # make a new blank image
-            new_image = np.empty((self.shape + (len(gradients),)))
+            #new_image = np.zeros((self.shape + (len(gradients),)))
             # populate the new image with the gradient
-            new_image[bounding_mask] = gradient_array
+            #new_image[bounding_mask] = gradient_array
+
+            gradients = [np.gradient(g) for g in np.rollaxis(self.pixels, -1)]
+            gradients = list(itertools.chain.from_iterable(gradients))
+            gradients = [g[..., None] for g in gradients]
+            gradient_array = np.concatenate(gradients, axis=-1)
+
+            from skimage.morphology import diamond, binary_erosion
+
+            a = diamond(1)
+
+            mask = binary_erosion(self.mask.pixels, a)
+
+            new_image = np.zeros((self.shape + (len(gradients),)))
+            new_image[mask] = gradient_array[mask]
+
         return Image(new_image, mask=self.mask)

pybug/transform/statisticallydriven.py
@@ -6,7 +6,8 @@ class StatisticallyDrivenTransform(Transform):
 
     #TODO: Rethink this transform so it knows how to deal with complex shapes
     def __init__(self, model, transform_constructor, source=None,
-                 parameters=None, global_transform=None, speed_up=None):
+                 parameters=None, global_transform=None,
+                 composition='linear', speed_up=None):
         """
         A transform that couples a traditional landmark-based transform to a
         statistical model together with a global similarity transform,
@@ -58,6 +59,10 @@ class StatisticallyDrivenTransform(Transform):
             global_transform = global_transform.from_vector(
                 self.global_parameters)
         self.global_transform = global_transform
+
+        # composition
+        self.composition = composition
+        self.compose = self._select_composition(self.composition)
 
         # speed up
         if speed_up is not None:
@@ -162,6 +167,7 @@ class StatisticallyDrivenTransform(Transform):
                                             parameters=flattened,
                                             global_transform=self
                                             .global_transform,
+                                            composition=self.composition,
                                             speed_up=(self._cached_points,
                                                       self.dW_dX))
 
@@ -171,7 +177,18 @@ class StatisticallyDrivenTransform(Transform):
     def _apply(self, x, **kwargs):
         return self.transform._apply(x, **kwargs)
 
-    def compose(self, statistically_driven_transform):
+    def _select_composition(self, composition):
+        if composition is 'linear':
+            return self._compose_linear
+        elif composition is 'algorithmic':
+            return self._compose_algorithmic
+        elif composition is 'mathematical':
+            return self._compose_mathematical
+        else:
+            raise ValueError('Unknown optimisation string selected. Valid'
+                             'options are: GN, LM')
+
+    def _compose_mathematical(self, statistically_driven_transform):
         """
         Composes two statistically driven transforms together based on the
         first order approximation proposed in:
@@ -229,11 +246,12 @@ class StatisticallyDrivenTransform(Transform):
         #.linear_component.T)
 
         dW_dx = self.transform.jacobian_points(self.source)
+        dW_dx = np.eye(2, 2)
         #dW_dx = np.dot(dW_dx, self.global_transform.linear_component.T)
         dW_dx = dW_dx[np.newaxis, ...]
         # dW_dx:  n_landmarks  x  n_dim  x  n_dim
 
-        dW_dx_dW_dp_0 = np.einsum('ilj, idj -> idj', dW_dx, dW_dp_0)
+        dW_dx_dW_dp_0 = np.einsum('ijl, idl -> idj', dW_dx, dW_dp_0)
 
         #dW_dx.swapaxes(1, 2)
         #aux1 = dW_dx[:, :, 0]
@@ -259,6 +277,35 @@ class StatisticallyDrivenTransform(Transform):
 
         return self.from_vector(p)
 
+    def _compose_linear(self, statistically_driven_transform):
+        new_mean = statistically_driven_transform.target
+
+        aux3 = self.model.template_sample.from_vector(
+            np.dot(self.model.components.T,
+                   self.weights)).points + \
+            new_mean.points
+
+        aux4 = self.global_transform.apply(aux3)
+
+        from pybug.shape import PointCloud
+        return self.estimate(PointCloud(aux4))
+
+    def _compose_algorithmic(self, statistically_driven_transform):
+        pass
+
+    def compose(self):
+        pass
+
+    def estimate(self, target):
+        global_transform = self.global_transform.estimate(
+            self.model.mean.points, target.points)
+        global_parameters = global_transform.as_vector()
+        aligned_target = global_transform.inverse.apply(target)
+        weights = self.model.project(aligned_target)
+        parameters = np.hstack((global_parameters, weights))
+        return self.from_vector(parameters)
+
     @property
     def inverse(self):
+
         return self.from_vector(-self.as_vector())