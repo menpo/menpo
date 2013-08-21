09f20b382a032999e7574632e77eacc2a98349de
Joan Alabort <ja310@imperial.ac.uk>
2013-08-21 19:02:48 +0100

    Consistent interface for PWA

--- pybug/transform/piecewiseaffine.py
+++ pybug/transform/piecewiseaffine.py
@@ -113,7 +113,7 @@ class PiecewiseAffineTransform(Transform):
         alpha, beta = self.alpha_beta(points)
         return self._containment_from_alpha_beta(alpha, beta)
 
-    def jacobian_source(self, points):
+    def weight_points(self, points):
         """
         Returns the jacobian of the warp at each
         :param points:
@@ -157,6 +157,14 @@ class PiecewiseAffineTransform(Transform):
             jac[points_index_in_tri_i, vertices_index_of_tri_i[2], :] = (
                 gamma_k[points_index_in_tri_i, i][..., None])
         return jac
+
+    def jacobian_points(self, points):
+        """
+        Returns the jacobian of the warp at each
+        :param points:
+        :return:
+        """
+        return np.eye(2, 2)
 
     def _tri_containment_loop(self, points):
         """
