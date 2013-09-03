fcd2c4245179ac4f474f2fcc3371b6b312144687
James Booth <jabooth@gmail.com>
2013-09-03 15:45:42 +0100

    expanded tcoords_pixel_scaled and documentation

--- pybug/shape/mesh/textured.py
+++ pybug/shape/mesh/textured.py
@@ -31,17 +31,35 @@ class TexturedTriMesh(TriMesh):
 
     def tcoords_pixel_scaled(self):
         r"""
-        Returns a PointCloud that is scaled to be suitable for directly
+        Returns a PointCloud that is modified to be suitable for directly
         indexing into the pixels of the texture (e.g. for manual mapping
-        operations).
+        operations). The resulting tcoords behave just like image landmarks
+        do:
+
+         >>> texture = texturedtrimesh.texture
+         >>> tc_ps = texturedtrimesh.tcoords_pixel_scaled()
+         >>> pixel_values_at_tcs = texture[tc_ps[: ,0], tc_ps[:, 1]]
+
+        The operations that are performed are:
+
+        - Flipping the origin from bottom-left to top-left
+        - Scaling the tcoords by the image shape (denormalising them)
+        - Permuting the axis so that
 
         Returns
         -------
         tcoords_scaled : :class:`pybug.shape.PointCloud`
-            A scaled version of the tcoords.
+            A copy of the tcoords that behave like Image landmarks
         """
         scale = Scale(np.array(self.texture.shape)[::-1])
-        return PointCloud(scale.apply(self.tcoords.points))
+        tcoords = self.tcoords.points.copy()
+        # flip the 'y' st 1 -> 0 and 0 -> 1, moving the axis to upper left
+        tcoords[:, 1] = 1 - tcoords[:, 1]
+        # apply the scale to get the units correct
+        tcoords = scale.apply(tcoords)
+        # flip axis 0 and axis 1 so indexing is as expected
+        tcoords = tcoords[:, ::-1]
+        return PointCloud(tcoords)
 
     def _view(self, figure_id=None, new_figure=False, textured=True, **kwargs):
         r"""
