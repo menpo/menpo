c84ab2d80cd0f1e7fbafab4a4263f3834cd37237
Joan Alabort <ja310@imperial.ac.uk>
2013-09-10 10:40:31 +0100

    Changed __init__.py in package align

--- pybug/align/__init__.py
+++ pybug/align/__init__.py
@@ -1,23 +1,2 @@
-from pybug.align.lucaskanade import \
-    (AdaptiveForwardAdditive,
-     AdaptiveForwardCompositional,
-     AdaptiveInverseCompositional,
-     AlternatingForwardAdditive,
-     AlternatingForwardCompositional,
-     AlternatingInverseCompositional,
-     ProjectOutForwardAdditive,
-     ProjectOutForwardCompositional,
-     ProjectOutInverseCompositional,
-     SimultaneousForwardAdditive,
-     SimultaneousForwardCompositional,
-     SimultaneousInverseCompositional,
-     ImageForwardAdditive,
-     ImageForwardCompositional,
-     ImageInverseCompositional,
-     LSIntensity,
-     ECC,
-     GaborFourier,
-     GradientImages,
-     GradientCorrelation)
 from pybug.align.nonrigid import TPS, MultipleTPS
 from pybug.align.rigid import GeneralizedProcrustesAnalysis, Procrustes
