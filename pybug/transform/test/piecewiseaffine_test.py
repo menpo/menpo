a8af802d9f2607d98963a6b83b32e952f5353789
James Booth <jabooth@gmail.com>
2013-09-10 20:38:22 +0100

    fix to the piecewiseaffine_test

--- pybug/transform/test/piecewiseaffine_test.py
+++ pybug/transform/test/piecewiseaffine_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
-from pybug.transform.piecewiseaffine import AbstractPWATransform
+from pybug.transform.piecewiseaffine import PiecewiseAffineTransform, \
+    DiscreteAffinePWATransform
 
 
 a = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
@@ -19,12 +20,12 @@ b_affine = np.array(
 
 
 def test_affine_transforms():
-    pwa = AbstractPWATransform(a, b, tl)
+    pwa = DiscreteAffinePWATransform(a, b, tl)
     assert(len(pwa.transforms) == 2)
     assert_equal(pwa.transforms[0].homogeneous_matrix, a_affine)
     assert_equal(pwa.transforms[1].homogeneous_matrix, b_affine)
 
 
 def test_pwa_n_parameters():
-    pwa = AbstractPWATransform(a, b, tl)
+    pwa = PiecewiseAffineTransform(a, b, tl)
     assert(pwa.n_parameters == 12)
