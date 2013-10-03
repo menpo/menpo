0c463e6430f80818b56b47e2d9ce6085c05e6a22
James Booth <jabooth@gmail.com>
2013-10-03 16:19:42 +0100

    SDT is now tidier, seperation global out

--- pybug/transform/statisticallydriven.py
+++ pybug/transform/statisticallydriven.py
@@ -5,6 +5,402 @@ from pybug.transform.base import AlignableTransform
 class StatisticallyDrivenTransform(AlignableTransform):
     r"""
     A transform that couples a traditional landmark-based transform to a
+    statistical model such that source points of the alignment transform
+    are the points of the model. The parameters of the transform are just
+    the weights of statistical model.
+
+    If no source is provided, the mean of the model is defined as the
+    source landmarks of the transform.
+
+    Parameters
+    ----------
+    model : :class:`pybug.model.base.StatisticalModel`
+        A linear statistical shape model.
+    transform_cls : :class:`pybug.transform.AlignableTransform`
+        A class of :class:`pybug.transform.base.AlignableTransform`
+        The align constructor will be called on this with the source
+        and target landmarks. The target is
+        set to the points generated from the model using the
+        provide weights - the source is either given or set to the
+        model's mean.
+    source : :class:`pybug.shape.base.PointCloud`
+        The source landmarks of the transform. If no ``source`` is provided the
+        mean of the model is used.
+    weights : (P,) ndarray
+        The reconstruction weights that will be fed to the model in order to
+        generate an instance of the target landmarks.
+    composition: 'both', 'warp' or 'model', optional
+        The composition approximation employed by this
+        StatisticallyDrivenTransform.
+
+        Default: 'both'
+    """
+    #TODO: Rethink this transform so it knows how to deal with complex shapes
+    def __init__(self, model, transform_cls, source=None, weights=None,
+                 composition='both'):
+        super(StatisticallyDrivenTransform, self).__init__()
+
+        self._cached_points = None
+        self.model = model
+        self.composition = composition
+        if source is None:
+            # set the source to the model's mean
+            source = self.model.mean
+        self._source = source
+
+        if weights is None:
+            # set all weights to 0 (yielding the mean)
+            weights = np.zeros(self.model.n_components)
+        self._weights = weights
+
+        self._target = self._target_for_weights(self._weights)
+        # by providing _source and _target we conform to the
+        # AlignmentTransform interface
+        # utilize the align constructor to build the transform
+        self.transform = transform_cls.align(self.source, self.target)
+
+    @property
+    def n_dims(self):
+        r"""
+        The number of dimensions that the transform supports.
+
+        :type: int
+        """
+        return self.transform.n_dims
+
+    @property
+    def n_parameters(self):
+        r"""
+        The total number of parameters.
+
+        Simply ``n_weights``.
+
+        :type: int
+        """
+        return self.n_weights
+
+    @property
+    def n_weights(self):
+        r"""
+        The number of parameters in the linear model.
+
+        :type: int
+        """
+        return self.model.n_components
+
+
+    @property
+    def has_true_inverse(self):
+        return False
+
+    def _build_pseudoinverse(self):
+        return self.from_vector(-self.as_vector())
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @weights.setter
+    def weights(self, value):
+        r"""
+        Setting the weights value automatically triggers a recalculation of
+        the target, and an update of the transform
+        """
+        self._weights = value
+        self.target = self._target_for_weights(value)
+
+    def jacobian(self, points):
+        """
+        Calculates the Jacobian of the StatisticallyDrivenTransform wrt to
+        its parameters (the weights). This is done by chaining the relative
+        weight of each point wrt the source landmarks, i.e. the Jacobian of
+        the warp wrt the source landmarks when the target is assumed to be
+        equal to the source (dW/dx), together with the Jacobian of the
+        linear model  wrt its weights (dX/dp).
+
+        Parameters
+        -----------
+        points: (N, D) ndarray
+            The points at which the Jacobian will be evaluated.
+
+        Returns
+        -------
+        dW/dp : (N, P, D) ndarray
+            The Jacobian of the StatisticallyDrivenTransform evaluated at the
+            previous points.
+        """
+        # check if re-computation of dW/dx can be avoided
+        if not np.array_equal(self._cached_points, points):
+            # recompute dW/dx, i.e. the relative weight of each point wrt
+            # the source landmarks
+            self.dW_dX = self.transform.weight_points(points)
+            # cache points
+            self._cached_points = points
+
+        # dX/dp is simply the Jacobian of the model
+        dX_dp = self.model.jacobian
+
+        # dW_dX:    n_points   x    n_points    x  n_dims
+        # dX_dp:  n_points  x     n_params      x  n_dims
+        dW_dp = np.einsum('ild, lpd -> ipd', self.dW_dX, dX_dp)
+        # dW_dp:    n_points   x     n_params      x  n_dims
+
+        return dW_dp
+
+    # TODO: document me
+    def jacobian_points(self, points):
+        r"""
+        TO BE DOCUMENTED
+
+        Returns
+        -------
+        dW_dx : (N, D, D) ndarray
+            The jacobian with respect to the points
+        """
+        pass
+
+    def as_vector(self):
+        r"""
+        Return the current parameters of this transform - this is the
+        just the linear model's weights
+
+        Returns
+        -------
+        params : (``n_parameters``,) ndarray
+            The vector of parameters
+        """
+        return self.weights
+
+    def update_from_vector(self, vector):
+        r"""
+        Updates the StatisticallyDrivenTransform's state from it's
+        vectorized form.
+        """
+        self.weights = vector[self.n_global_parameters:]
+
+    def _update_from_target(self, new_target):
+        r"""
+        On a new target being set, we need to:
+
+        1. Find the optimum weights that align the model to this target,
+        and set them as self.weights.
+
+        2. Update the transform to point to the closest target that the
+        model can provide to the requested target
+
+        3. Set our target to the closest target that the model can provide
+        to the requested target.
+
+        Parameters
+        ----------
+
+        new_target: :class:`PointCloud`
+            The new_target that we want to set.
+        """
+        # 1. Find the optimum weights and set them
+        self._weights = self._weights_for_target(new_target)
+        # 2. Find the closest target the model can reproduce and trigger an
+        # update of our transform
+        self.transform.target = self._target_for_weights(self.weights)
+        # 3. As always, update our self._target
+        self._target = self.transform.target
+
+    def _apply(self, x, **kwargs):
+        r"""
+        Apply this transform to the given object. Uses the internal transform.
+
+        Parameters
+        ----------
+        x : (N, D) ndarray or a transformable object
+            The object to be transformed.
+        kwargs : dict
+            Passed through to transforms ``apply`` method.
+
+        Returns
+        --------
+        transformed : (N, D) ndarray or object
+            The transformed object
+        """
+        return self.transform._apply(x, **kwargs)
+
+    # TODO: Could be implemented as optimization option in LK???
+    # Problems:
+    #   - This method needs to be explicitly overwritten in order to match
+    #     the common interface defined for AlignableTransform objects
+    def compose(self, statistically_driven_transform):
+        if self.composition is 'model':
+            # TODO this seems to be the same, revisit
+            return self._compose_warp(statistically_driven_transform)
+        elif self.composition is 'warp':
+            new_target = self._compose_warp(statistically_driven_transform
+            .target)
+            return self.from_target(new_target)
+        elif self.composition is 'both':
+            new_params = self._compose_both(statistically_driven_transform
+            .as_vector())
+            return self.from_vector(new_params)
+        else:
+            raise ValueError('Unknown composition string selected. Valid'
+                             'options are: model, warp, both')
+
+    def compose_from_vector_inplace(self, sdt_parameters):
+        r"""
+        Compose this StatisticallyDrivenTransform with another inplace.
+        Rather than requiring a new StatisticallyDrivenTransform to compose
+        with, this method only requires the parameters of the new transform.
+
+        Parameters
+        ----------
+
+        sdt_parameters: (P,) ndarray
+            The parameters of the StatisticallyDrivenTransform that we are
+            composing with, as found from as_vector().
+
+        """
+        if self.composition is 'model':
+            # TODO this seems to be the same, revisit
+            temp_sdt = self.from_vector(sdt_parameters)
+            self.target = self._compose_warp(temp_sdt.target)
+        elif self.composition is 'warp':
+            temp_sdt = self.from_vector(sdt_parameters)
+            self.target = self._compose_warp(temp_sdt.target)
+        elif self.composition is 'both':
+            self.update_from_vector(self._compose_both(sdt_parameters))
+        else:
+            raise ValueError('Unknown composition string selected. Valid'
+                             'options are: model, warp, both')
+
+    # TODO: The call to transform.apply will not work properly for PWA
+    #   - Define a new function in TPS & PWA called .apply_to_target
+    #   - For TPS this function should ne the same as the normal .apply()
+    #     method
+    #   - For PWA it should implement Bakers algorithmic approach to
+    #     composition
+    def _compose_warp(self, other_target):
+        r"""
+        Composes two statistically driven transforms together. This approach
+        composes the
+
+        Parameters
+        ----------
+        other_target : :class:`PointCloud`
+            the target of the StatisticallyDrivenTransform we are
+            composing with.
+
+        Returns
+        -------
+
+        target: :class:`PointCloud`
+            The new target of the composed result
+        """
+        return self.transform.apply(other_target)
+
+    def _compose_both(self, new_sdt_parameters):
+        r"""
+        Composes two statistically driven transforms together based on the
+        first order approximation proposed by Papandreou and Maragos.
+
+        Parameters
+        ----------
+        new_sdt_parameters : (P,) ndarray
+            the parameters of the StatisticallyDrivenTransform we are
+            composing with, as provided by .as_vector().
+
+        Returns
+        -------
+
+        parameters: (P,) ndarray
+            The new parameters of the composed result
+
+        References
+        ----------
+
+        .. [1] G. Papandreou and P. Maragos, "Adaptive and Constrained
+               Algorithms for Inverse Compositional Active Appearance Model
+               Fitting", CVPR08
+        """
+        model_jacobian = self.model.jacobian
+
+        # compute:
+        # -> dW/dp when p=0
+        # -> dW/dp when p!=0
+        # -> dW/dx when p!=0 evaluated at the source landmarks
+
+        # dW/dp when p=0 and when p!=0 are the same and simply given by
+        # the Jacobian of the model
+        dW_dp_0 = model_jacobian
+        dW_dp = dW_dp_0
+        # dW_dp_0:  n_points  x     n_params     x  n_dims
+        # dW_dp:    n_points  x     n_params     x  n_dims
+
+        dW_dx = self.transform.jacobian_points(self.model.mean.points)
+        # dW_dx:  n_points  x  n_dims  x  n_dims
+
+        #TODO: Can we do this without splitting across the two dimensions?
+        dW_dx_x = dW_dx[:, 0, :].flatten()[..., None]
+        dW_dx_y = dW_dx[:, 1, :].flatten()[..., None]
+        dW_dp_0_mat = np.reshape(dW_dp_0, (self.model.mean.n_points * self.n_dims,
+                                           self.n_parameters))
+        dW_dx_dW_dp_0 = dW_dp_0_mat * dW_dx_x + dW_dp_0_mat * dW_dx_y
+        dW_dx_dW_dp_0 = np.reshape(dW_dx_dW_dp_0, (self.model.mean.n_points,
+                                                   self.n_parameters,
+                                                   self.n_dims))
+        # dW_dx:          n_points  x  n_dims    x  n_dims
+        # dW_dp_0:        n_points  x  n_params  x  n_dims
+        # dW_dx_dW_dp_0:  n_points  x  n_params  x  n_dims
+
+        J = np.einsum('ijk, ilk -> jl', dW_dp, dW_dx_dW_dp_0)
+        H = np.einsum('ijk, ilk -> jl', dW_dp, dW_dp)
+
+        Jp = np.linalg.solve(H, J)
+        # Jp:  n_params  x  n_params
+
+        return self.as_vector() + np.dot(Jp, new_sdt_parameters)
+
+    def _target_for_weights(self, weights):
+        r"""
+        Return the appropriate target for the model weights provided.
+        Subclasses can override this.
+
+        Parameters
+        ----------
+
+        weights: (P,) ndarray
+            weights of the statistical model that should be used to generate a
+            new instance
+
+        Returns
+        -------
+
+        new_target: :class:`pybug.shape.PointCloud`
+            A new target for the weights provided
+        """
+        return self.model.instance(weights)
+
+    def _weights_for_target(self, target):
+        r"""
+        Return the appropriate model weights for target provided.
+        Subclasses can override this.
+
+        Parameters
+        ----------
+
+        target: :class:`pybug.shape.PointCloud`
+            The target that the statistical model will try to reproduce
+
+        Returns
+        -------
+
+        weights: (P,) ndarray
+            Weights of the statistical model that generate the closest
+            PointCloud to the requested target
+        """
+        return self.model.project(target)
+
+
+class StatisticallyDrivenAndGlobalTransform(AlignableTransform):
+    r"""
+    A transform that couples a traditional landmark-based transform to a
     statistical model together with a global similarity transform,
     such that the parameters of the transform are fully specified by
     both the weights of statistical model and the parameters of the
