b4de79ecfc44c5f7f14a06cda02ba3ad2e59870e
Epameinondas Antonakos <antonakosn@gmail.com>
2013-11-28 15:46:26 +0000

    delted lbp

--- pybug/features/cpp/LBP.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "WindowFeature.h"
-
-class LBP : public WindowFeature {
-public:
-	LBP();
-	virtual ~LBP();
-	void apply(double *windowImage, unsigned int windowHeight, unsigned int windowWidth);
-};
