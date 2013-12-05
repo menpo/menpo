880cd99f89f1b96581bdc7afa134a99ac642c633
patricksnape <patricksnape@gmail.com>
2013-12-05 18:25:00 +0000

    Better tests!

--- pybug/transform/test/tps_test.py
+++ pybug/transform/test/tps_test.py
@@ -61,22 +61,20 @@ def test_tps_jacobian_unitary_x_y():
 def test_tps_jacobian_manual_sample_a():
     tps = TPS(src, tgt_perturbed)
     dW_dxy = tps.jacobian_source(square_sample_points)
-    onetwothreefour = np.array(
-        [[0.78665966, 0.62374388],
-         [0.09473867, -0.68910365],
-         [0.68788235, 0.18713078],
-         [-0.80552584, 1.11078411]])
-    assert_allclose(dW_dxy[1234], onetwothreefour)
+    onetwothreefour = np.array([[0.79641476, 0.54082559],
+                                [0.11218034, 0.03472567],
+                                [0.02858224, 0.01380962],
+                                [-0.00093218, 0.07808401]])
+    assert_allclose(dW_dxy[1234], onetwothreefour, rtol=10 ** -6)
 
 
 def test_tps_jacobian_manual_sample_b():
     tps = TPS(src, tgt_perturbed)
     dW_dxy = tps.jacobian_source(square_sample_points)
-    threesixfouronethree = np.array(
-        [[0.67244171, -0.0098011],
-         [-0.77028611, -1.19256858],
-         [0.8296718, 0.95940495],
-         [-0.03122042, 1.00073478]])
+    threesixfouronethree = np.array([[0.09406137, 0.05216822],
+                                     [-0.00402943, -0.01160705],
+                                     [0.88186413, 1.01594663],
+                                     [0.02749696, 0.08572215]])
     assert_allclose(dW_dxy[36413], threesixfouronethree, atol=1e-5)
 
 
@@ -104,9 +102,10 @@ def test_tps_build_pseudoinverse():
 def test_tps_apply():
     src = PointCloud(np.array([[-1.0, -1.0], [-1, 1], [1, -1], [1, 1]]))
     tgt = PointCloud(np.array([[-2.0, -2.0], [-2, 2], [2, -2], [2, 2]]))
+    pts = PointCloud(np.array([[-0.1, -1.0], [-0.5, 1.0], [2.1, -2.5]]))
     tps = TPS(src, tgt)
-    result = tps.apply(tgt_perturbed)
-    expected = np.array([[-1.2, -2.6], [-1.6, 2.4], [1.4, -1.6], [2.6, 1]])
+    result = tps.apply(pts)
+    expected = np.array([[-0.2, -2.], [-1., 2.], [4.2, -5.]])
     assert_allclose(result.points, expected)
 
 
@@ -114,7 +113,7 @@ def test_tps_jacobian_points():
     src = PointCloud(np.array([[-1.0, -1.0], [-1, 1], [1, -1], [1, 1]]))
     tgt = PointCloud(np.array([[-2.0, -2.0], [-2, 2], [2, -2], [2, 2]]))
     tps = TPS(src, tgt)
-    result = tps.jacobian_points(src)
+    result = tps.jacobian_points(src.points)
 
     assert_allclose(result, np.array([[[2, 0], [0., 2]],
                                       [[2, 0], [0, 2]],
@@ -125,22 +124,30 @@ def test_tps_jacobian_points():
 def test_tps_jacobian_source():
     src = PointCloud(np.array([[-1.0, -1.0], [-1, 1], [1, -1], [1, 1]]))
     tgt = PointCloud(np.array([[-2.0, -2.0], [-2, 2], [2, -2], [2, 2]]))
+    pts = np.array([[-0.1, -1.0], [-0.5, 1.0], [2.1, -2.5]])
     tps = TPS(src, tgt)
-    result = tps.jacobian_source(src.points)
-    assert_allclose(result, np.array([[[2., 2.], [0, 0], [0, 0], [0, 0]],
-                                      [[0, 0], [2., 2.], [0, 0], [0, 0]],
-                                      [[0, 0], [0, 0], [2., 2.], [0, 0]],
-                                      [[0, 0], [0, 0], [0, 0], [2., 2.]]]),
-                    atol=10 ** -14)
+    result = tps.jacobian_source(pts)
+    expected = np.array([[[1.10799034, 1.10799034], [-0.00799034, -0.00799034],
+                          [0.89200966, 0.89200966], [0.00799034, 0.00799034]],
+                         [[-0.0325033, -0.0325033], [1.5325033, 1.5325033],
+                          [0.0325033, 0.0325033], [0.4674967, 0.4674967]],
+                         [[0.03263195, 0.03263195], [-1.13263195, -1.13263195],
+                          [3.46736805, 3.46736805],
+                          [-0.36736805, -0.36736805]]])
+    assert_allclose(result, expected, rtol=10 ** -6)
 
 
 def test_tps_weight_points():
     src = PointCloud(np.array([[-1.0, -1.0], [-1, 1], [1, -1], [1, 1]]))
     tgt = PointCloud(np.array([[-2.0, -2.0], [-2, 2], [2, -2], [2, 2]]))
+    pts = np.array([[-0.1, -1.0], [-0.5, 1.0], [2.1, -2.5]])
     tps = TPS(src, tgt)
-    result = tps.weight_points(src.points)
-    assert_allclose(result, np.array([[[1., 1.], [0, 0], [0, 0], [0, 0]],
-                                      [[0, 0], [1., 1.], [0, 0], [0, 0]],
-                                      [[0, 0], [0, 0], [1., 1.], [0, 0]],
-                                      [[0, 0], [0, 0], [0, 0], [1., 1.]]]),
-                    atol=10 ** -14)
+    result = tps.weight_points(pts)
+    expected = np.array([[[0.55399517, 0.55399517], [-0.00399517, -0.00399517],
+                          [0.44600483, 0.44600483], [0.00399517, 0.00399517]],
+                         [[-0.01625165, -0.01625165], [0.76625165, 0.76625165],
+                          [0.01625165, 0.01625165], [0.23374835, 0.23374835]],
+                         [[0.01631597, 0.01631597], [-0.56631597, -0.56631597],
+                          [1.73368403, 1.73368403],
+                          [-0.18368403, -0.18368403]]])
+    assert_allclose(result, expected, rtol=10 ** -6)
