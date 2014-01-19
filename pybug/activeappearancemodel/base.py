3f5518a07c91255c3a4a6a69dbfc69e4be3af2bf
Joan Alabort <ja310@imperial.ac.uk>
2014-01-19 23:58:48 +0000

    Restructure amm package

--- pybug/activeappearancemodel/base.py
+++ pybug/activeappearancemodel/base.py
@@ -1,19 +1,21 @@
 from __future__ import division
 import numpy as np
 from copy import deepcopy
-import matplotlib.pylab as plt
-from pybug.transform .affine import Scale, SimilarityTransform, \
-    UniformScale, AffineTransform
+import matplotlib.pyplot as plt
+from pybug.shape import TriMesh
+from pybug.transform .affine import Scale, SimilarityTransform, UniformScale, \
+    AffineTransform, Translation
+from pybug.groupalign import GeneralizedProcrustesAnalysis
 from pybug.transform.modeldriven import OrthoMDTransform
 from pybug.transform.piecewiseaffine import PiecewiseAffineTransform
 from pybug.transform.tps import TPS
+from pybug.model import PCAModel
 from pybug.lucaskanade.residual import LSIntensity
 from pybug.lucaskanade.appearance import AlternatingInverseCompositional
-from pybug.activeappearancemodel.accuracy import compute_error_facesize
-from pybug.activeappearancemodel.builder import \
-    build_reference_frame, build_patch_reference_frame, \
-    rescale_to_reference_landmarks, gaussian_pyramid, compute_features, \
-    noisy_align
+from pybug.activeappearancemodel.functions import mean_pointcloud,\
+    build_reference_frame, build_patch_reference_frame, compute_features,\
+    compute_error_facesize, compute_error_me17, compute_error_p2p, \
+    compute_error_rms
 
 
 class AAM(object):
@@ -28,13 +30,12 @@ class AAM(object):
     appearance_model_pyramid:
     """
 
-    def __init__(self, shape_model, reference_frame,
-                 appearance_model_pyramid, features):
-        self.shape_model = shape_model
-        self.reference_frame = reference_frame
+    def __init__(self, shape_model_pyramid, appearance_model_pyramid,
+                 features_dic, downscale):
+        self.shape_model_pyramid = shape_model_pyramid
         self.appearance_model_pyramid = appearance_model_pyramid
-        self.features = features
-        self._lk_pyramid = None
+        self.features_dic = features_dic
+        self.downscale = downscale
 
     @property
     def n_levels(self):
@@ -42,30 +43,26 @@ class AAM(object):
 
     def _feature_pyramid(self, image, reference_landmarks):
         r"""
-        Fit the AAM to an image using Lucas-Kanade.
+        Compute to an image using Lucas-Kanade.
 
         Parameters
         -----------
-        image:
+        image: :class:`pybug.image.IntensityImage`
+            The image to which the
 
         Returns
         -------
         feature_pyramid:
         """
-        # rescale object to reference resolution
-        #image = rescale_to_reference_landmarks(image, reference_landmarks)
-        # build gaussian pyramid and reverse it
-        image_pyramid = gaussian_pyramid(
-            image, n_levels=self.n_levels)
-        image_pyramid.reverse()
-        # compute features
-        return [compute_features(i, self.features['type'],
-                                 **self.features['options'])
-                for i in image_pyramid]
+        image = image.rescale_to_reference_landmarks(reference_landmarks)
+        pyramid = image.gaussian_pyramid(n_levels=self.n_levels)
+        image_pyramid = [compute_features(p.next(), self.features_dic)
+                         for p in pyramid]
+        return image_pyramid.reverse()
 
     def instance(self, shape_weights, appearance_weights, level=-1,
                  transform_cls=PiecewiseAffineTransform, patches=False,
-                 patch_size=[16, 16], interpolator='scipy', **warp_kwargs):
+                 patch_size=(16, 16), interpolator='scipy', **warp_kwargs):
         r"""
         Create a novel AAM instance using the given shape and appearance
         weights.
@@ -106,18 +103,28 @@ class AAM(object):
         if patches:
             transform_cls = TPS
 
+        template = self.appearance_model_pyramid[level].mean
+        landmarks = template.landmarks['source'].lms
+
         # multiply weights by std
         n_shape_weights = len(shape_weights)
-        shape_weights *= self.shape_model.eigenvalues[:n_shape_weights] ** 0.5
+        shape_weights *= self.shape_model_pyramid[level].eigenvalues[
+            :n_shape_weights] ** 0.5
         # compute shape instance
-        shape_instance = self.shape_model.instance(shape_weights)
+        shape_instance = \
+            self.shape_model_pyramid[level].instance(shape_weights)
         # build reference frame using to shape instance
         if patches:
             reference_frame = build_patch_reference_frame(
                 shape_instance, patch_size=patch_size)
 
         else:
-            reference_frame = build_reference_frame(shape_instance)[0]
+            if type(landmarks) == TriMesh:
+                trilist = template.landmarks['source'].lms.trilist
+            else:
+                trilist = None
+            reference_frame = build_reference_frame(shape_instance,
+                                                    trilist=trilist)
 
         # select appearance model
         n_appearance_weights = len(appearance_weights)
@@ -130,9 +137,8 @@ class AAM(object):
 
         # compute transform mapping appearance instance to previous
         # reference frame
-        transform = transform_cls(
-            reference_frame.landmarks['source'].lms,
-            self.reference_frame.landmarks['source'].lms)
+        transform = transform_cls(reference_frame.landmarks['source'].lms,
+                                  landmarks)
         # return warped appearance
         return appearance_instance.warp_to(reference_frame.mask, transform,
                                            interpolator, **warp_kwargs)
@@ -219,14 +225,14 @@ class AAM(object):
             md_transform = lk.align(i, lk.transform.as_vector(),
                                     max_iters=max_iters)
             md_transform_pyramid.append(md_transform)
-            target = Scale(2, n_dims=md_transform.n_dims).apply(
+            target = Scale(self.downscale, n_dims=md_transform.n_dims).apply(
                 md_transform.target)
 
         return deepcopy(md_transform_pyramid)
 
-    def lk_fit_landmarked_image(self, image, runs=10, noise_std=0.05,
-                               group='PTS', max_iters=20, verbose=True,
-                               view=False):
+    def lk_fit_landmarked_image(self, image, runs=5, noise_std=0.05,
+                                group='PTS', max_iters=20, verbose=True,
+                                view=False):
         r"""
         Fit the AAM to an image using Lucas-Kanade.
 
@@ -315,63 +321,232 @@ class AAM(object):
 
         return optimal_transforms
 
-    # def initialize_regression(self, levels=None,
-    #                           md_transform_cls=OrthoMDTransform,
-    #                           transform_cls=PiecewiseAffineTransform,
-    #                           global_transform_cls=SimilarityTransform,
-    #                           regression_features=parameters,
-    #                           n_shape=None, n_appearance=None):
-    #     r"""
-    #
-    #     Parameters
-    #     -----------
-    #     md_transform_cls: :class:`pybug.transform.base.modeldriven`
-    #
-    #         Default: OrthoMDTransform
-    #
-    #     transform_cls: :class:`pybug.transform.base.PureAlignmentTransform`
-    #
-    #         Default: PieceWiseAffineTransform
-    #
-    #     global_transform_cls: :class:`pybug.transform.base.affine`
-    #
-    #         Default: SimilarityTransform
-    #
-    #     regression_features:
-    #
-    #         Default: parameters
-    #
-    #     n_shape:
-    #
-    #         Default: None
-    #
-    #     n_appearance:
-    #
-    #         Default: None
-    #
-    #     """
-    #     self._regression_pyramid = []
-    #
-    #     for l in levels:
-    #
-    #         self.regression_appearance_models.append(self
-    #         .appearance_model_pyramid[l])
-    #
-    #         global_transform = global_transform_cls(np.eye(3, 3))
-    #         source = self.reference_frame.landmarks['source'].lms
-    #
-    #         sm = self.shape_model
-    #         if n_shape is not None:
-    #             sm.n_active_components = n_s
-    #         md_transform = md_transform_cls(sm,
-    #                                         transform_cls,
-    #                                         global_transform,
-    #                                         source=source)
-    #
-    #         am = deepcopy(am)
-    #         if n_appearance is not None:
-    #             am.n_active_components = n_a
-    #
-    #         self._lk_pyramid.append(lk_algorithm(am, residual(),
-    #                                              md_transform))
 
+# TODO: Should this be a method on SimilarityTransform? AlignableTransforms?
+def noisy_align(source, target, noise_std=0.05, rotation=False):
+    r"""
+    Constructs and perturbs the optimal similarity transform between source
+    to the target by adding white noise to its parameters.
+
+    Parameters
+    ----------
+    source: :class:`pybug.shape.PointCloud`
+        The source pointcloud instance used in the alignment
+
+    target: :class:`pybug.shape.PointCloud`
+        The target pointcloud instance used in the alignment
+
+    noise_std: float
+        The standard deviation of the white noise
+
+        Default: 0.05
+    rotation: boolean
+        If False the second parameter of the SimilarityTransform,
+        which captures captures inplane rotations, is set to 0.
+
+        Default:False
+
+    Returns
+    -------
+    noisy_transform : :class: `pybug.transform.SimilarityTransform`
+        The noisy Similarity Transform
+    """
+    transform = SimilarityTransform.align(source, target)
+    parameters = transform.as_vector()
+    if not rotation:
+        parameters[1] = 0
+    parameter_range = np.hstack((parameters[:2], target.range()))
+    noise = (parameter_range * noise_std *
+             np.random.randn(transform.n_parameters))
+    parameters += noise
+    return SimilarityTransform.from_vector(parameters)
+
+
+def aam_builder(images, group=None, label='all', interpolator='scipy',
+                reference_shape=None, scale=1,
+                boundary=3, trilist=None,
+                patches=False, patch_size=(16, 16), n_levels=3,
+                scaled_reference_frames=False, downscale=2,
+                transform_cls=PiecewiseAffineTransform,
+                features_dic=None,
+                max_shape_components=None, max_appearance_components=None):
+
+    r"""
+
+    Parameters
+    ----------
+    images: list
+        The images from which to build the Active Appearance Model
+    group: str, optional
+
+        Default: 'PTS'
+    label: tr, optional
+
+        Default: 'all'
+    interpolator: str, optional
+
+        Default: 'scipy'
+    reference_landmarks: optional
+
+        Default: None
+    scale: float, optional
+
+        Default: 1
+    crop_boundary: float, optional
+
+        Default: 0.2
+    reference_frame_boundary: int, optional
+
+        Default: 3
+    trilist: (Nt, 3) ndarray, optional
+
+        Default: None
+    n_levels: int, optional
+
+        Default: 3
+    transform_cls: optional
+
+        Default: PieceWiseAffine
+    feature_space: dictionary, optional
+
+        Default: None
+    max_shape_components: float, optional
+
+        Default: 0.95
+    max_appearance_components: float, optional
+
+        Deafult: 0.95
+
+    Returns
+    -------
+    aam : :class:`pybug.activeappearancemodel.AAM`
+    """
+
+    if reference_shape is None:
+        print '- Computing reference shape'
+        shapes = [i.landmarks[group][label].lms for i in images]
+        reference_shape = mean_pointcloud(shapes)
+
+    print '- Rescaling images to reference shape'
+    images = [i.rescale_to_reference_landmarks(reference_shape,
+                                               group=group, label=label,
+                                               interpolator=interpolator)
+              for i in images]
+
+    print '- Setting gaussian pyramid generators'
+    pyramids = [i.gaussian_pyramid(n_levels=n_levels, downscale=downscale)
+                for i in images]
+
+    print '- Computing features'
+    images = [compute_features(p.next(), features_dic)
+              for p in pyramids]
+    # extract potentially rescaled shapes
+    shapes = [i.landmarks[group][label].lms for i in images]
+
+    print '- Building shape model'
+    # centralize shapes
+    centered_shapes = [Translation(-s.centre).apply(s) for s in shapes]
+    # align centralized shape using Procrustes Analysis
+    gpa = GeneralizedProcrustesAnalysis(centered_shapes)
+    aligned_shapes = [s.aligned_source for s in gpa.transforms]
+    # scale shapes if necessary
+    if scale != 1:
+        aligned_shapes = [Scale(scale, n_dims=reference_shape.n_dims).apply(s)
+                          for s in aligned_shapes]
+
+    # build shape model
+    shape_model = PCAModel(aligned_shapes)
+    if max_shape_components is not None:
+        # trim shape model if required
+        shape_model.trim_components(max_shape_components)
+
+    print '- Building reference frame'
+    reference_shape = mean_pointcloud(aligned_shapes)
+    if patches:
+        # build patch based reference frame
+        reference_frame = build_patch_reference_frame(
+            reference_shape, boundary=boundary, patch_size=patch_size)
+    else:
+        # build reference frame
+        reference_frame = build_reference_frame(
+            reference_shape, boundary=boundary, trilist=trilist)
+
+    print '- Building model pyramids'
+    shape_model_pyramid = []
+    appearance_model_pyramid = []
+    # for each level
+    for j in range(n_levels):
+        print ' - Level {}'.format(j)
+
+        if j != 0:
+            print ' - Computing features'
+            images = [compute_features(p.next(), features_dic)
+                      for p in pyramids]
+
+            print ' - Building shape model'
+            if scaled_reference_frames:
+                shapes = [i.landmarks[group][label].lms for i in images]
+                centered_shapes = [Translation(-s.centre).apply(s)
+                                   for s in shapes]
+                gpa = GeneralizedProcrustesAnalysis(centered_shapes)
+                aligned_shapes = [s.aligned_source for s in gpa.transforms]
+                if scale != 1:
+                    aligned_shapes = \
+                        [Scale(scale, n_dims=reference_shape.n_dims).apply(s)
+                         for s in aligned_shapes]
+                # build shape model
+                shape_model = PCAModel(aligned_shapes)
+                if max_shape_components is not None:
+                    # trim shape model if required
+                    shape_model.trim_components(max_shape_components)
+
+                print ' - Building reference frame'
+                reference_shape = mean_pointcloud(aligned_shapes)
+                if patches:
+                    # build patch based reference frame
+                    reference_frame = build_patch_reference_frame(
+                        reference_shape, boundary=boundary, patch_size=patch_size)
+                else:
+                    # build reference frame
+                    reference_frame = build_reference_frame(
+                        reference_shape, boundary=boundary, trilist=trilist)
+
+        # add shape model to the list
+        shape_model_pyramid.append(shape_model)
+
+        print ' - Computing transforms'
+        transforms = [transform_cls(reference_frame.landmarks['source'].lms,
+                                    i.landmarks[group][label].lms)
+                      for i in images]
+
+        print ' - Warping images'
+        images = [i.warp_to(reference_frame.mask, t,
+                            interpolator=interpolator)
+                  for i, t in zip(images, transforms)]
+
+        for i in images:
+            i.landmarks['source'] = reference_frame.landmarks['source']
+        if patches:
+            for i in images:
+                i.build_mask_around_landmarks(patch_size, group=group,
+                                              label=label)
+        else:
+            for i in images:
+                i.constrain_mask_to_landmarks(group='source', trilist=trilist)
+
+        print ' - Building appearance model'
+        appearance_model = PCAModel(images)
+        # trim appearance model if required
+        if max_appearance_components is not None:
+            appearance_model.trim_components(max_appearance_components)
+
+        # add appearance model to the list
+        appearance_model_pyramid.append(appearance_model)
+
+    # reverse the list of shape and appearance models so that they are
+    # ordered from lower to higher resolution
+    shape_model_pyramid.reverse()
+    appearance_model_pyramid.reverse()
+
+    return AAM(shape_model_pyramid, appearance_model_pyramid,
+               features_dic, downscale)
--- pybug/activeappearancemodel/functions.py
+++ pybug/activeappearancemodel/functions.py
@@ -1,6 +1,158 @@
 from __future__ import division
 import matplotlib.pylab as plt
 import numpy as np
+from pybug.shape import PointCloud, TriMesh
+from pybug.landmark.labels import labeller
+from pybug.transform .affine import Translation
+from pybug.image import MaskedNDImage
+
+
+def build_reference_frame(landmarks, boundary=3, group='source',
+                          trilist=None):
+    r"""
+    Build reference frame from landmarks.
+
+    Parameters
+    ----------
+    reference_landmarks:
+    scale: int, optional
+
+        Default: 1
+    boundary: int, optional
+
+        Default: 3
+    group: str, optional
+
+        Default: 'source'
+    trilist: (Nt, 3) ndarray, optional
+
+        Default: None
+
+    Returns
+    -------
+    rescaled_image : type(self)
+        A copy of this image, rescaled.
+    """
+    reference_frame = _build_reference_frame(landmarks, boundary=boundary,
+                                             group=group)
+    if trilist is not None:
+        reference_frame.landmarks[group] = TriMesh(
+            reference_frame.landmarks['source'].lms.points, trilist=trilist)
+
+    # TODO: revise kwarg trilist in method constrain_mask_to_landmarks,
+    # perhaps the trilist should be directly obtained from the group landmarks
+    reference_frame.constrain_mask_to_landmarks(group=group, trilist=trilist)
+
+    return reference_frame
+
+
+def build_patch_reference_frame(landmarks, boundary=3, group='source',
+                                patch_size=(16, 16)):
+    r"""
+    Build reference frame from landmarks.
+
+    Parameters
+    ----------
+    reference_landmarks:
+    scale: int, optional
+
+        Default: 1
+    boundary: int, optional
+
+        Default: 3
+    group: str, optional
+
+        Default: 'source'
+    patch_size: dictionary, optional
+
+        Default: None
+
+    Returns
+    -------
+    rescaled_image : type(self)
+        A copy of this image, rescaled.
+    """
+    boundary = np.max(patch_size) + boundary
+    reference_frame = _build_reference_frame(landmarks, boundary=boundary,
+                                             group=group)
+
+    # mask reference frame
+    reference_frame.build_mask_around_landmarks(patch_size, group=group)
+
+    return reference_frame
+
+
+def _build_reference_frame(landmarks, boundary=3, group='source'):
+    # translate landmarks to the origin
+    minimum = landmarks.bounds(boundary=boundary)[0]
+    landmarks = Translation(-minimum).apply(landmarks)
+
+    resolution = landmarks.range(boundary=boundary)
+    reference_frame = MaskedNDImage.blank(resolution)
+    reference_frame.landmarks[group] = landmarks
+
+    return reference_frame
+
+
+def compute_features(image, features_dic):
+    r"""
+    Compute feature images.
+
+    Parameters
+    ----------
+    image: :class:`pybug.image.MaskedNDImage`
+        The original image from which the features will be computed
+    features_dic: dic
+        ['type'] : string
+            String specifying the type of features to compute
+        ['options']: **kwargs:
+            Passed through to the particular feature method being used. See
+            `pybug/image/MaskedNDImage` for details on feature options.
+
+    Returns
+    -------
+    feature_image: :class:`pybug.image.MaskedNDImage`
+        The resulting feature image.
+    """
+    if features_dic is not None:
+        if features_dic['type'] is 'normalize_std':
+            image.normalize_std_inplace(**features_dic['options'])
+        elif features_dic['type'] is 'normalize_norm':
+            image.normalize_norm_inplace(**features_dic['options'])
+        elif features_dic['type'] is 'euler':
+            raise NotImplementedError("Euler features not implemented yet")
+        elif features_dic['type'] is 'igo':
+            raise NotImplementedError("IGO features not implemented yet")
+        elif features_dic['type'] is 'es':
+            raise NotImplementedError("ES features not implemented yet")
+        elif features_dic['type'] is 'hog':
+            raise NotImplementedError("HoG features not implemented yet")
+        elif features_dic['type'] is 'sift':
+            raise NotImplementedError("Sift features not implemented yet")
+
+    # TODO: These should disappear with the new image refactoring
+    nd_image = MaskedNDImage(image.pixels, mask=image.mask)
+    nd_image.landmarks = image.landmarks
+
+    return nd_image
+
+
+def mean_pointcloud(pointcloud_list):
+    r"""
+    Compute the mean of a list of Point Cloud objects
+
+    Parameters
+    ----------
+    pointcloud_list: list of :class:`pybug.shape.pointcloud`
+        List of PointCloud objects from which we want to
+        compute the mean..
+
+    Returns
+    -------
+    mean_pointcloud: class:`pybug.shape.pointcloud`
+        The mean PointCloud
+    """
+    return PointCloud(np.mean([pc.points for pc in pointcloud_list], axis=0))
 
 
 def compute_error_rms(fitted, ground_truth):
