4e2ee5c33bd2c791af4312d960b771c79892c952
Joan Alabort <ja310@imperial.ac.uk>
2014-02-03 09:28:21 +0000

    Deletes unnecessary functions from function.py Please enter the commit message for your changes. Lines starting

--- pybug/aam/functions.py
+++ pybug/aam/functions.py
@@ -194,135 +194,27 @@ def mean_pointcloud(pointcloud_list):
     return PointCloud(np.mean([pc.points for pc in pointcloud_list], axis=0))
 
 
-def compute_error_rms(fitted, ground_truth):
-    return np.sqrt(np.mean((fitted.flatten() - ground_truth.flatten()) ** 2))
+def compute_error(target, ground_truth, error_type='me_norm'):
+    if error_type is 'me_norm':
+        return _compute_me_norm(target, ground_truth)
+    elif error_type is 'me':
+        return _compute_me(target, ground_truth)
+    elif error_type is 'rmse':
+        return _compute_rmse(target, ground_truth)
+    else:
+        raise ValueError("Unknown error_type string selected. Valid options "
+                         "are: me_norm, me, rmse'")
 
 
-def compute_error_p2p(fitted, ground_truth):
-    return np.mean(np.sqrt(np.sum((fitted - ground_truth) ** 2, axis=-1)))
+def _compute_rmse(target, ground_truth):
+    return np.sqrt(np.mean((target.flatten() - ground_truth.flatten()) ** 2))
 
 
-def compute_error_facesize(fitted, ground_truth):
-    face_size = np.mean(np.max(ground_truth, axis=0) -
-                        np.min(ground_truth, axis=0))
-    return compute_error_p2p(fitted, ground_truth) / face_size
+def _compute_me(target, ground_truth):
+    return np.mean(np.sqrt(np.sum((target - ground_truth) ** 2, axis=-1)))
 
 
-def compute_error_me17(fitted, ground_truth, leye, reye):
-    return (compute_error_p2p(fitted, ground_truth) /
-            compute_error_p2p(leye, reye))
-
-
-def cumulative_error_distribution(fitted_shapes, original_shape,
-                                  error_type='face_size', label=None):
-    r"""
-    Plots Cumulative Error Distribution (CED)
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-
-    """
-
-    if error_type is 'rms':
-        errors = [compute_error_rms(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-    elif error_type is 'p2p':
-        errors = [compute_error_p2p(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-    elif error_type is 'face_size':
-        errors = [compute_error_facesize(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-
-        plt.xlabel('point-to-point error normalized by face size')
-        plt.ylabel('proportion of images')
-
-    elif error_type is 'me17':
-        errors = [compute_error_me17(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-
-    n_shapes = len(fitted_shapes)
-    error_axis = np.arange(0, stop, step)
-    proportion_axis = [np.count_nonzero(errors < limit) / n_shapes
-                       for limit in error_axis]
-
-    error_median = np.median(errors)
-    error_mean = np.mean(errors)
-    error_std = np.std(errors)
-
-    text = label + '  median = {}  mean = {}  std = {}'.format(
-        error_median, error_mean, error_std)
-
-    plt.plot(error_axis, proportion_axis, label=text)
-
-    plt.title('Cumulative Error Distribution')
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-
-    return errors, error_median, error_mean, error_std
-
-
-def error_histogram(fitted_shapes, original_shape, error_type='face_size',
-                    label=None):
-    r"""
-    Plots Error Histogram (EH)
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-
-    """
-
-    if error_type is 'rms':
-        errors = [compute_error_rms(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-    elif error_type is 'p2p':
-        errors = [compute_error_p2p(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-    elif error_type is 'face_size':
-        errors = [compute_error_facesize(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-
-        plt.xlabel('point-to-point error normalized by face size')
-        plt.ylabel('proportion of images')
-
-    elif error_type is 'me17':
-        errors = [compute_error_me17(f.points, o.points)
-                  for f, o in zip(fitted_shapes, original_shape)]
-        stop = 0.1
-        step = 0.001
-
-    error_median = np.median(errors)
-    error_mean = np.mean(errors)
-    error_std = np.std(errors)
-
-    text = label + '  median = {}  mean = {}  std = {}'.format(
-        error_median, error_mean, error_std)
-
-    plt.hist(errors, 50, histtype='step', label=text)
-
-    plt.title('Cumulative Error Distribution')
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-
-    return errors, error_median, error_mean, error_std
+def _compute_me_norm(target, ground_truth):
+    normalizer = np.mean(np.max(ground_truth, axis=0) -
+                         np.min(ground_truth, axis=0))
+    return _compute_me(target, ground_truth) / normalizer
