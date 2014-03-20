b59782a3673f58db8e40e0846f9ff7071d23341b
patricksnape <patricksnape@gmail.com>
2014-03-20 12:23:16 +0000

    Use the latest pyvrml fork

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(name='menpo',
                         'scikit-image>=0.8.2',
 
                         # 3D import
-                        'menpo-pyvrml97>=2.3',
+                        'menpo-pyvrml97',
 
                         # Visualization
                         'matplotlib>=1.2.1',
