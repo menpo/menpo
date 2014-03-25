9fae2644f0a9ee46d9b351c580a5eb2b3707dae1
James Booth <jabooth@gmail.com>
2014-03-25 16:52:58 +0000

    extract and pca in MMBuilder

--- menpo/mm/base.py
+++ menpo/mm/base.py
@@ -3,6 +3,7 @@ from menpo.groupalign import GeneralizedProcrustesAnalysis
 from menpo.shape import PointCloud
 from menpo.transform.tps import TPS
 from menpo.transform import Translation, Scale
+from menpo.model import PCAModel
 from menpo.rasterize import GLRasterizer
 from menpo.rasterize.transform import (ExtractNDims, AddNDims,
                                        CylindricalUnwrapTransform)
@@ -40,29 +41,62 @@ def mean_pointcloud(pointclouds):
     return PointCloud(sum(pointclouds) / len(pointclouds))
 
 
+def build_trimesh_extractor(sample_image, sampling_rate=50):
+    import numpy as np
+    x, y = np.meshgrid(np.arange(0, sample_image.height, sampling_rate),
+                       np.arange(0, sample_image.width, sampling_rate))
+    # use the sample_image's mask to filter which points should be
+    # selectable
+    sample_in_range = sample_image.mask.pixels[x, y]
+    v_x = x[sample_in_range[..., 0]]
+    v_y = y[sample_in_range[..., 0]]
+
+    # build a cheeky TriMesh to get hassle free trilist
+    from menpo.shape import TriMesh
+    tm = TriMesh(np.vstack([v_x, v_y]).T)
+
+    def extract_trimesh(shape_image):
+        sampled = shape_image.pixels[v_x, v_y, :]
+        return TriMesh(sampled, trilist=tm.trilist.copy())
+
+    return extract_trimesh
+
+
 class MMBuilder(object):
 
     def __init__(self, models, group=None, label='all'):
         self.models = models
         self.group = group
         self.label = label
+
+        print 'Rigidly aligning...'
         # ra = Rigid Aligned
         self.ra_models, self.ra_mean_lms, self.unwrapper = None, None, None
-        print 'Rigidly aligning...'
         self.rigidly_align()
-        # u = Unwrapped (infers ra) 
+
+        print 'Unwrapping and flattening...'
+        # u = Unwrapped (infers ra)
         self.u_models, self.u_mean_lms = None, None
         self.u_2d, self.u_mean_lms_2d = None, None
-        print 'Unwrapping and flattening...'
         self.unwrap_and_flatten()
+
+        print 'Warping...'
         # w = Warped (infers ra + u)
         self.w_models_2d = None
-        print 'Warping...'
         self.warp()
+
+        print 'Rasterizing...'
         self.r = GLRasterizer()
         self.shape_images = []
-        print 'Rasterizing...'
         self.rasterize()
+
+        print 'Extracting TriMeshes...'
+        self.dc_meshes = []
+        self.extract()
+
+        print 'Taking PCA...'
+        self.shape_model = None
+        self.pca()
 
     def lms_for(self, x):
         return x.landmarks[self.group][self.label].lms
@@ -91,13 +125,31 @@ class MMBuilder(object):
 
     def rasterize(self):
         trans_to_clip_space = clip_space_transform(self.u_mean_lms_2d)
-        cs_w_models_2d = [trans_to_clip_space.apply(m)
-                          for m in self.w_models_2d]
+        cs_models_2d = [trans_to_clip_space.apply(m) for m in self.w_models_2d]
+        cs_mean_lms_2d = trans_to_clip_space.apply(self.u_mean_lms_2d)
         add_nill_z = AddNDims(1)  # adds an all-zero z axis
-        cs_w_models = [add_nill_z.apply(m) for m in cs_w_models_2d]
+        cs_models = [add_nill_z.apply(m) for m in cs_models_2d]
+        mean_lm_img = self.r.model_to_image_space(
+            add_nill_z.apply(cs_mean_lms_2d))
 
+        # build all the shape images
         self.shape_images = []
-        for cs_w_model, orig in zip(cs_w_models, self.ra_models):
+        for cs_w_model, orig in zip(cs_models, self.ra_models):
             blank, shape_image = self.r.rasterize_mesh_with_f3v_interpolant(
                 cs_w_model, per_vertex_f3v=orig.points)
+            shape_image.landmarks['mean'] = mean_lm_img
             self.shape_images.append(shape_image)
+
+        print 'masking to landmarks...'
+        # set the mask correctly on all meshes
+        si = self.shape_images[0]
+        si.constrain_mask_to_landmarks()
+        for s in self.shape_images:
+            s.mask.pixels = si.mask.pixels.copy()
+
+    def extract(self):
+        tm_extract = build_trimesh_extractor(self.shape_images[0])
+        self.dc_meshes = [tm_extract(s) for s in self.shape_images]
+
+    def pca(self):
+        self.shape_model = PCAModel(self.dc_meshes)
