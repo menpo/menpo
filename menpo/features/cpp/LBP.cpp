6a0d3664936bbdada05e8d4737621872c6b69d66
Nontas Antonakos <nontas@nontas-laptop.home>
2014-03-31 23:42:12 +0100

    precomputes samples coords! makes lbps faster

--- menpo/features/cpp/LBP.cpp
+++ menpo/features/cpp/LBP.cpp
@@ -2,9 +2,9 @@
 
 LBP::LBP(unsigned int windowHeight, unsigned int windowWidth, unsigned int numberOfChannels,
          unsigned int *radius, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
-         unsigned int mapping_type, unsigned int *uniqueSamples, unsigned int *whichMappingTable, unsigned int numberOfUniqueSamples) {
+         unsigned int mapping_type, unsigned int *uniqueSamples, unsigned int *whichMappingTable,
+         unsigned int numberOfUniqueSamples) {
 	unsigned int descriptorLengthPerWindow = numberOfRadiusSamplesCombinations * numberOfChannels;
-    this->radius = radius;
     this->samples = samples;
     this->whichMappingTable = whichMappingTable;
     this->numberOfRadiusSamplesCombinations = numberOfRadiusSamplesCombinations;
@@ -26,49 +26,60 @@ LBP::LBP(unsigned int windowHeight, unsigned int windowWidth, unsigned int numbe
         }
 	}
     this->mapping_tables = mapping_tables;
+
+    // find coordinates of the window centre in the window reference frame (axes origin in bottom left corner)
+    double centre_y = (windowHeight-1)/2;
+    double centre_x = (windowWidth-1)/2;
+
+    // find samples coordinates for each radius/samples combination
+    // in the window reference frame (axes origin in bottom left corner)
+    double **samples_x_tables, **samples_y_tables, angle_step;
+    unsigned int s;
+    samples_x_tables = new double*[numberOfRadiusSamplesCombinations];
+    samples_y_tables = new double*[numberOfRadiusSamplesCombinations];
+    for (i=0; i<numberOfRadiusSamplesCombinations; i++) {
+        samples_x_tables[i] = new double[samples[i]];
+        samples_y_tables[i] = new double[samples[i]];
+        angle_step = 2*PI/samples[i];
+        for (s=0; s<samples[i]; s++) {
+            samples_x_tables[i][s] = centre_x + radius[i] * cos(s * angle_step);
+            samples_y_tables[i][s] = centre_y - radius[i] * sin(s * angle_step);
+        }
+    }
+    this->samples_x_tables = samples_x_tables;
+    this->samples_y_tables = samples_y_tables;
 }
 
 LBP::~LBP() {
+    // empty memory
     delete [] mapping_tables;
+    delete [] samples_x_tables;
+    delete [] samples_y_tables;
 }
 
 
 void LBP::apply(double *windowImage, double *descriptorVector) {
-    LBPdescriptor(windowImage, this->radius, this->samples, this->numberOfRadiusSamplesCombinations, this->whichMappingTable,
-                  this->mapping_tables, this->windowHeight, this->windowWidth, this->numberOfChannels, descriptorVector);
+    LBPdescriptor(windowImage, this->samples, this->numberOfRadiusSamplesCombinations, this->samples_x_tables, this->samples_y_tables,
+                  this->whichMappingTable, this->mapping_tables, this->windowHeight, this->windowWidth, this->numberOfChannels, descriptorVector);
 }
 
 
-void LBPdescriptor(double *inputImage, unsigned int *radius, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
-                   unsigned int *whichMappingTable, unsigned int **mapping_tables, unsigned int imageHeight, unsigned int imageWidth,
+void LBPdescriptor(double *inputImage, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
+                   double **samples_x_tables, double **samples_y_tables, unsigned int *whichMappingTable,
+                   unsigned int **mapping_tables, unsigned int imageHeight, unsigned int imageWidth,
                    unsigned int numberOfChannels, double *descriptorVector) {
-    unsigned int i, s, ch, max_samples;
+    unsigned int i, s, ch;
     int centre_y, centre_x, rx, ry, fx, fy, cx, cy;
-    double angle_step, centre_val, sample_val;
+    double centre_val, sample_val;
     double tx, ty, w1, w2, w3, w4;
     int lbp_code;
-
-    // initialize samples coordinates arrays and mapping array
-    max_samples = samples[0];
-    for (i=1; i<numberOfRadiusSamplesCombinations; i++) {
-        if (samples[i] > max_samples)
-            max_samples = samples[i];
-    }
-    double *samples_x = new double[max_samples];
-    double *samples_y = new double[max_samples];
 
     // find coordinates of the window centre in the window reference frame (axes origin in bottom left corner)
     centre_y = (int)((imageHeight-1)/2);
     centre_x = (int)((imageWidth-1)/2);
 
+    // for each radius/samples combination
     for (i=0; i<numberOfRadiusSamplesCombinations; i++) {
-        // find coordinates of sampling point in the window reference frame (axes origin in bottom left corner)
-        angle_step = 2*PI/samples[i];
-        for (s=0; s<samples[i]; s++) {
-            samples_x[s] = centre_x + radius[i] * cos(s * angle_step);
-            samples_y[s] = centre_y - radius[i] * sin(s * angle_step);
-        }
-
         // for each channel, compute the lbp code
         for (ch=0; ch<numberOfChannels; ch++) {
             // value of centre
@@ -76,17 +87,17 @@ void LBPdescriptor(double *inputImage, unsigned int *radius, unsigned int *sampl
             lbp_code = 0;
             for (s=0; s<samples[i]; s++) {
                 // check if interpolation is needed
-                rx = (int)round(samples_x[s]);
-                ry = (int)round(samples_y[s]);
-                if ( (fabs(samples_x[s] - rx) < small_val) && (fabs(samples_y[s] - ry) < small_val) )
+                rx = (int)round(samples_x_tables[i][s]);
+                ry = (int)round(samples_y_tables[i][s]);
+                if ( (fabs(samples_x_tables[i][s] - rx) < small_val) && (fabs(samples_y_tables[i][s] - ry) < small_val) )
                     sample_val = inputImage[ry + rx*imageHeight + ch*imageHeight*imageWidth];
                 else {
-                    fx = (int)floor(samples_x[s]);
-                    fy = (int)floor(samples_y[s]);
-                    cx = (int)ceil(samples_x[s]);
-                    cy = (int)ceil(samples_y[s]);
-                    tx = samples_x[s] - fx;
-                    ty = samples_y[s] - fy;
+                    fx = (int)floor(samples_x_tables[i][s]);
+                    fy = (int)floor(samples_y_tables[i][s]);
+                    cx = (int)ceil(samples_x_tables[i][s]);
+                    cy = (int)ceil(samples_y_tables[i][s]);
+                    tx = samples_x_tables[i][s] - fx;
+                    ty = samples_y_tables[i][s] - fy;
                     // compute interpolation weights and value
                     w1 = (1 - tx) * (1 - ty);
                     w2 =      tx  * (1 - ty);
@@ -107,9 +118,6 @@ void LBPdescriptor(double *inputImage, unsigned int *radius, unsigned int *sampl
             descriptorVector[i + ch*numberOfRadiusSamplesCombinations] = mapping_tables[whichMappingTable[i]][lbp_code];
         }
     }
-    // Empty memory
-    delete [] samples_x;
-    delete [] samples_y;
 }
 
 int power2(int index) {
@@ -143,6 +151,7 @@ void generate_codes_mapping_table(unsigned int *mapping_table, unsigned int mapp
     else if (mapping_type == 2) {
         // rotation invariant
         int rm, r;
+        unsigned int j;
         int *tmp_map = new int[power2(n_samples)];
         for (c=0; c<power2(n_samples); c++)
             tmp_map[c] = -1;
@@ -150,7 +159,7 @@ void generate_codes_mapping_table(unsigned int *mapping_table, unsigned int mapp
         for (c=0; c<power2(n_samples); c++) {
             rm = c;
             r = c;
-            for (int j=1; j<n_samples; j++) {
+            for (j=1; j<n_samples; j++) {
                 r = leftRotate(r, n_samples, 1);
                 if (r < rm)
                     rm = r;
@@ -197,5 +206,5 @@ int count_bits(int n) {
 int leftRotate(int num, unsigned int len_bits, unsigned int move_bits) {
    /* In num<<move_bits, last move_bits bits are 0. To put first 3 bits of num at
      last, do bitwise or of num<<move_bits with num >>(len_bits - move_bits) */
-   return (num << move_bits % len_bits) & (power2(len_bits) - 1) | ((num & (power2(len_bits) - 1)) >> (len_bits - (move_bits % len_bits)));
+   return ((num << move_bits % len_bits) & (power2(len_bits) - 1)) | ((num & (power2(len_bits) - 1)) >> (len_bits - (move_bits % len_bits)));
 }
--- menpo/features/cpp/LBP.h
+++ menpo/features/cpp/LBP.h
@@ -14,18 +14,21 @@ using namespace std;
 class LBP: public WindowFeature {
 public:
 	LBP(unsigned int windowHeight, unsigned int windowWidth, unsigned int numberOfChannels,
-	        unsigned int *radius, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations, unsigned int mapping_type,
-	        unsigned int *uniqueSamples, unsigned int *whichMappingTable, unsigned int numberOfUniqueSamples);
+	    unsigned int *radius, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
+	    unsigned int mapping_type, unsigned int *uniqueSamples, unsigned int *whichMappingTable,
+	    unsigned int numberOfUniqueSamples);
 	virtual ~LBP();
 	void apply(double *windowImage, double *descriptorVector);
 private:
-    unsigned int *radius, *samples, *whichMappingTable, **mapping_tables;
+    unsigned int *samples, *whichMappingTable, **mapping_tables;
     unsigned int numberOfRadiusSamplesCombinations, windowHeight, windowWidth, numberOfChannels;
+    double **samples_x_tables, **samples_y_tables;
 };
 
-void LBPdescriptor(double *inputImage, unsigned int *radius, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
-                   unsigned int *whichMappingTable, unsigned int **mapping_tables, unsigned int imageHeight,
-                   unsigned int imageWidth, unsigned int numberOfChannels, double *descriptorVector);
+void LBPdescriptor(double *inputImage, unsigned int *samples, unsigned int numberOfRadiusSamplesCombinations,
+                   double **samples_x_tables, double **samples_y_tables, unsigned int *whichMappingTable,
+                   unsigned int **mapping_tables, unsigned int imageHeight, unsigned int imageWidth,
+                   unsigned int numberOfChannels, double *descriptorVector);
 int power2(int index);
 void generate_codes_mapping_table(unsigned int *mapping_table, unsigned int mapping_type, unsigned int n_samples);
 int count_bit_transitions(int a, unsigned int n_samples);
