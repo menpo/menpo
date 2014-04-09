69ffe1ba4ec3fa8adaa64256dd8ee9e084277c04
Joan Alabort <ja310@imperial.ac.uk>
2014-04-09 11:49:49 +0100

    Rearranged methods in PDM
    - Also got rid of the composable methods

--- menpo/transform/pdm.py
+++ menpo/transform/pdm.py
@@ -1,15 +1,12 @@
 from copy import deepcopy
 import numpy as np
-from menpo.base import Targetable, Vectorizable
+from menpo.base import Targetable, VectorizableUpdatable
 from menpo.model import Similarity2dInstanceModel
 from .base import VInvertible
 
-# global tranform should be set in _new_target_for_state
-# properties should be changed for subclasses
-
 
 # TODO: document me
-class PDM(Targetable, Vectorizable, VInvertible):
+class PDM(Targetable, VectorizableUpdatable, VInvertible):
     r"""
     """
     def __init__(self, model, weights=None):
@@ -24,6 +21,32 @@ class PDM(Targetable, Vectorizable, VInvertible):
         #  of this two calls) here
         self._target_setter(self._new_target_from_state())
         self._sync_state_from_target()
+
+    @property
+    def n_dims(self):
+        r"""
+        The number of dimensions that the transform supports.
+
+        :type: int
+        """
+        return self.model.template_instance.n_dims
+
+    @property
+    def n_weights(self):
+        r"""
+        The number of parameters in the linear model.
+
+        :type: int
+        """
+        return self.model.n_active_components
+
+    @property
+    def weights(self):
+        r"""
+        In this simple PDM the weights is just the vector, but in subclasses
+        this behavior will change.
+        """
+        return self._weights
 
     @property
     def target(self):
@@ -77,23 +100,30 @@ class PDM(Targetable, Vectorizable, VInvertible):
         """
         return self.model.project(target)
 
-    @property
-    def n_dims(self):
+    def as_vector(self):
         r"""
-        The number of dimensions that the transform supports.
+        Return the current parameters of this transform - this is the
+        just the linear model's weights
 
-        :type: int
+        Returns
+        -------
+        params : (``n_parameters``,) ndarray
+            The vector of parameters
         """
-        return self.model.template_instance.n_dims
+        return self.weights
 
-    @property
-    def n_weights(self):
+    def from_vector_inplace(self, vector):
         r"""
-        The number of parameters in the linear model.
-
-        :type: int
+        Updates the ModelDrivenTransform's state from it's
+        vectorized form.
         """
-        return self.model.n_active_components
+        self.set_target(vector)
+
+    def update_from_vector_inplace(self, delta):
+        r"""
+        Additively update this object with a delta vector inplace.
+        """
+        self.from_vector_inplace(self.as_vector() + delta)
 
     @property
     def has_true_inverse(self):
@@ -126,116 +156,11 @@ class PDM(Targetable, Vectorizable, VInvertible):
         # just have to negate the parameters!
         return -vector
 
-    @property
-    def weights(self):
-        r"""
-        In this simple PDM the weights is just the vector, but in subclasses
-        this behavior will change.
-        """
-        return self._weights
-
-    # def set_weights(self, value):
-    #     r"""
-    #     Setting the weights value automatically triggers a recalculation of
-    #     the target, and an update of the transform.
-    #
-    #     In this simple PDM the weights are the parameters, but in subclasses
-    #     this behavior will change.
-    #     """
-    #     self.from_vector_inplace(value)
-
     # TODO: document me
     def jacobian(self, points):
         """
         """
         return self.model.jacobian
-
-    def as_vector(self):
-        r"""
-        Return the current parameters of this transform - this is the
-        just the linear model's weights
-
-        Returns
-        -------
-        params : (``n_parameters``,) ndarray
-            The vector of parameters
-        """
-        return self.weights
-
-    def from_vector_inplace(self, vector):
-        r"""
-        Updates the ModelDrivenTransform's state from it's
-        vectorized form.
-        """
-        self.set_target(vector)
-
-    def composes_inplace_with(self):
-        return PDM
-
-    def _compose_before_inplace(self, transform):
-        r"""
-        a_orig = deepcopy(a)
-        a.compose_before_inplace(b)
-        a.apply(p) == b.apply(a_orig.apply(p))
-
-        a is permanently altered to be the result of the composition. b is
-        left unchanged.
-
-        Parameters
-        ----------
-        transform : :class:`ModelDrivenTransform`
-            Transform to be applied **after** self
-
-        Returns
-        --------
-        transform : self
-            self, updated to the result of the composition
-        """
-        # naive approach - update self to be equal to transform and
-        # compose_before_from_vector_inplace
-        self_vector = self.as_vector().copy()
-        self.update_from_vector(transform.as_vector())
-        return self.compose_after_from_vector_inplace(self_vector)
-
-    # TODO: document me
-    def compose_after(self, transform):
-        r"""
-        c = a.compose_after(b)
-        c.apply(p) == a.apply(b.apply(p))
-
-        a and b are left unchanged.
-
-        This corresponds to the usual mathematical formalism for the compose
-        operator, `o`.
-
-        Parameters
-        ----------
-        transform : :class:`ModelDrivenTransform`
-            Transform to be applied **before** self
-
-        Returns
-        --------
-        transform : :class:`ModelDrivenTransform`
-            The resulting ModelDrivenTransform.
-        """
-        self_copy = deepcopy(self)
-        self_copy._compose_after_inplace(transform)
-        return self_copy
-
-    # TODO: document me
-    def _compose_after_inplace(self, pdm_transform):
-        r"""
-        """
-        new_params = self.compose_after_from_vector_inplace(
-            pdm_transform.as_vector())
-        return self.from_vector_inplace(new_params)
-
-    # TODO: document me
-    def compose_after_from_vector_inplace(self, vector):
-        r"""
-        """
-        self.weights = self.weights + vector
-        return self
 
 
 # TODO: document me
@@ -245,31 +170,6 @@ class GlobalPDM(PDM):
     def __init__(self, model, global_transform, weights=None):
         self.global_transform = global_transform
         super(GlobalPDM, self).__init__(model, weights=weights)
-
-    def _new_target_from_state(self):
-        r"""
-        Return the appropriate target for the model weights provided,
-        accounting for the effect of the global transform
-
-
-        Returns
-        -------
-
-        new_target: :class:`menpo.shape.PointCloud`
-            A new target for the weights provided
-        """
-        return self.global_transform.apply(self.model.instance(self.weights))
-
-    @property
-    def n_parameters(self):
-        r"""
-        The total number of parameters.
-
-        This is ``n_weights + n_global_parameters``.
-
-        :type: int
-        """
-        return self.n_weights + self.n_global_parameters
 
     @property
     def n_global_parameters(self):
@@ -289,54 +189,19 @@ class GlobalPDM(PDM):
         """
         return self.global_transform.as_vector()
 
-    def jacobian(self, points):
+    def _new_target_from_state(self):
         r"""
-        """
-        global_transform_jacobian = self._global_transform_jacobian(
-            self.model.mean.points)
-        return np.hstack((global_transform_jacobian.T,
-                          self.model.components.T))
+        Return the appropriate target for the model weights provided,
+        accounting for the effect of the global transform
 
-    def _global_transform_jacobian(self, points):
-        return self.global_transform.jacobian(points)
-
-    def as_vector(self):
-        r"""
-        Return the current parameters of this transform - this is the
-        just the linear model's weights
 
         Returns
         -------
-        params : (``n_parameters``,) ndarray
-            The vector of parameters
-        """
-        return np.hstack([self.global_parameters, self.weights])
 
-    def from_vector_inplace(self, vector):
-        global_parameters = vector[:self.n_global_parameters]
-        weights = vector[self.n_global_parameters:]
-        self._update_global_weights(global_parameters)
-        self._weights = weights
-        self._sync_target_from_state()
-
-    def _update_global_weights(self, global_weights):
-        r"""
-        Hook that allows for overriding behavior when the global weights are
-        set. Default implementation simply asks global_transform to
-        update itself from vector.
+        new_target: :class:`menpo.shape.PointCloud`
+            A new target for the weights provided
         """
-        self.global_transform.from_vector_inplace(global_weights)
-
-    # TODO: document me
-    def compose_after_from_vector_inplace(self, vector):
-        r"""
-        """
-        global_parameters = (self.global_parameters +
-                             vector[:self.n_global_parameters])
-        weights = self.weights + vector[self.n_global_parameters:]
-        self._update_global_weights(global_parameters)
-        self._weights = weights
-        return self
+        return self.global_transform.apply(self.model.instance(self.weights))
 
     def _weights_for_target(self, target):
         r"""
@@ -373,11 +238,45 @@ class GlobalPDM(PDM):
     def _update_global_transform(self, target):
         self.global_transform.target = target
 
+    def as_vector(self):
+        r"""
+        Return the current parameters of this transform - this is the
+        just the linear model's weights
+
+        Returns
+        -------
+        params : (``n_parameters``,) ndarray
+            The vector of parameters
+        """
+        return np.hstack([self.global_parameters, self.weights])
+
+    def from_vector_inplace(self, vector):
+        global_parameters = vector[:self.n_global_parameters]
+        weights = vector[self.n_global_parameters:]
+        self._update_global_weights(global_parameters)
+        self._weights = weights
+        self._sync_target_from_state()
+
+    def _update_global_weights(self, global_weights):
+        r"""
+        Hook that allows for overriding behavior when the global weights are
+        set. Default implementation simply asks global_transform to
+        update itself from vector.
+        """
+        self.global_transform.from_vector_inplace(global_weights)
+
+    def jacobian(self, points):
+        r"""
+        """
+        return np.hstack((self.global_transform.jacobian(points).T,
+                          self.model.components.T))
+
 
 # TODO: document me
 class OrthoPDM(GlobalPDM):
     r"""
     """
+
     def __init__(self, model, global_transform, weights=None):
         # 1. Construct similarity model from the mean of the model
         self.similarity_model = Similarity2dInstanceModel(model.mean)
@@ -390,6 +289,15 @@ class OrthoPDM(GlobalPDM):
         super(OrthoPDM, self).__init__(
             model, global_transform, weights=weights)
 
+    @property
+    def global_parameters(self):
+        r"""
+        The parameters for the global transform.
+
+        :type: (``n_global_parameters``,) ndarray
+        """
+        return self.similarity_weights
+
     def _update_global_transform(self, target):
         self.similarity_weights = self.similarity_model.project(target)
         self._update_global_weights(self.similarity_weights)
@@ -399,14 +307,9 @@ class OrthoPDM(GlobalPDM):
         new_target = self.similarity_model.instance(global_weights)
         self.global_transform.target = new_target
 
-    def _global_transform_jacobian(self, points):
-        return self.similarity_model.components
-
-    @property
-    def global_parameters(self):
+    def jacobian(self, points):
         r"""
-        The parameters for the global transform.
-
-        :type: (``n_global_parameters``,) ndarray
         """
-        return self.similarity_weights
+        return np.hstack((self.similarity_model.components.T,
+                          self.model.components.T))
+
