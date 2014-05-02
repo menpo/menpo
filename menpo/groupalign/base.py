d949a54ecf6c366ec8cf745890574daad7791b88
Epameinondas Antonakos <antonakosn@gmail.com>
2014-05-02 18:28:10 +0100

    removes prints

--- menpo/groupalign/base.py
+++ menpo/groupalign/base.py
@@ -30,7 +30,6 @@ class MultipleAlignment(object):
         self.n_sources = len(sources)
         self.n_points, self.n_dims = sources[0].n_points, sources[0].n_dims
         self.sources = sources
-        print type(sources[0])
         if target is None:
             # set the target to the mean source position
             self.target = PointCloud(
@@ -38,4 +37,3 @@ class MultipleAlignment(object):
         else:
             assert self.n_dims, self.n_points == target.shape
             self.target = target
-        print type(self.target)
