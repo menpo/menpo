ba5530f9acfbd74bcd99dab46b5c72c9d2f24e49
James Booth <jabooth@gmail.com>
2014-05-08 08:50:49 +0100

    __init__.py still had multivariate_normal

--- menpo/math/__init__.py
+++ menpo/math/__init__.py
@@ -2,4 +2,3 @@ from .circlefit import circle_fit
 from .convolution import log_gabor
 from .decomposition import (eigenvalue_decomposition,
                             principal_component_decomposition)
-from .multivariate import multivariate_normal
