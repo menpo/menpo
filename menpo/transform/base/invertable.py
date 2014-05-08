ac85e59c39cf286b9caf98d2a1bd3b95335dbb66
Patrick Snape <patricksnape@gmail.com>
2014-05-08 16:09:02 +0100

    Update Invertable docs

--- menpo/transform/base/invertable.py
+++ menpo/transform/base/invertable.py
@@ -3,18 +3,17 @@ import abc
 
 class Invertible(object):
     r"""
-    Transform Mixin for invertible transforms. Provides an interface for
+    :map:`Transform` Mixin for invertible transforms. Provides an interface for
     taking the psuedo or true inverse of a transform.
     """
 
     @abc.abstractproperty
     def has_true_inverse(self):
         r"""
-        True if the pseudoinverse is an exact inverse.
+        `True` if the pseudoinverse is an exact inverse.
 
         :type: Boolean
         """
-        pass
 
     @property
     def pseudoinverse(self):
@@ -24,44 +23,42 @@ class Invertible(object):
         the transforms parameters. If the transform has a true inverse this
         is returned instead.
 
-        :type: :class:`Transform`
+        :type: :map:`Transform`
         """
         return self._build_pseudoinverse()
 
     @abc.abstractmethod
     def _build_pseudoinverse(self):
         r"""
-        Returns this transform's inverse if it has one. if not,
+        Returns this transform's inverse if it has one. If not,
         the pseduoinverse is given.
 
-        This method is called by the pseudoinverse property and must be
+        This method is called by the pseudoinverse property and **must** be
         overridden.
-
 
         Returns
         -------
-        pseudoinverse: type(self)
+        pseudoinverse : `type(self)`
+            The object representing the pseudoinverse of this object.
         """
-        pass
 
 
 class VInvertible(Invertible):
     r"""
-    Transform Mixin for Vectorizable invertible transforms.
+    Transform Mixin for :map:`Vectorizable` invertible transforms.
 
-    Prefer this Mixin over Invertible if the Transform in question is
-    Vectorizable as this adds from_vector variants to the Invertible
-    interface. These can be tuned for performance, and are for instance
-    needed by some of the machinery of AAMs.
+    Prefer this Mixin over :map:`Invertible` if the :map:`Transform` in question
+    is :map:`Vectorizable` as this adds :meth:`from_vector` variants to the
+    :map:`Invertible` interface. These can be tuned for performance, and are,
+    for instance, needed by some of the machinery of AAMs.
     """
 
     def pseudoinverse_vector(self, vector):
         r"""
         The vectorized pseudoinverse of a provided vector instance.
-
         Syntactic sugar for
 
-        self.from_vector(vector).pseudoinverse.as_vector()
+            self.from_vector(vector).pseudoinverse.as_vector()
 
         Can be much faster than the explict call as object creation can be
         entirely avoided in some cases.
@@ -69,7 +66,7 @@ class VInvertible(Invertible):
         Parameters
         ----------
         vector :  (P,) ndarray
-            A vectorized version of self
+            A vectorized version of `self`
 
         Returns
         -------
