8394d7716bd1be874d99a1fec47ed7394d8710ce
Patrick Snape <patricksnape@gmail.com>
2014-05-17 12:31:09 +0100

    Add tests for landmark copying

--- menpo/landmark/test/landmark_test.py
+++ menpo/landmark/test/landmark_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.testing import assert_allclose
+
 from menpo.landmark import LandmarkGroup, LandmarkManager
 from menpo.shape import PointCloud
 from menpo.testing import is_same_array
@@ -52,3 +53,38 @@ def test_LandmarkManager_set_pointcloud():
     assert (lgroup._target is target)
     assert (lgroup._pointcloud is not pcloud)
     assert_allclose(lgroup._labels_to_masks['all'], np.ones(10, dtype=np.bool))
+
+
+def test_landmarkgroup_copy_method():
+    points = np.ones((10, 3))
+    mask_dict = {'all': np.ones(10, dtype=np.bool)}
+    pcloud = PointCloud(points, copy=False)
+    target = PointCloud(points)
+
+    lgroup = LandmarkGroup(target, 'label', pcloud, mask_dict, copy=False)
+    lgroup_copy = lgroup.copy()
+
+    assert (not is_same_array(lgroup_copy._pointcloud.points,
+                              lgroup._pointcloud.points))
+    assert (lgroup_copy._target is lgroup._target)
+    # Check the mask dictionary is deepcopied properly
+    assert (lgroup._labels_to_masks is not lgroup_copy._labels_to_masks)
+    masks = zip(lgroup_copy._labels_to_masks.values(),
+                lgroup._labels_to_masks.values())
+    for ms in masks:
+        assert (ms[0] is not ms[1])
+
+
+def test_LandmarkManager_copy_method():
+    points = np.ones((10, 3))
+    pcloud = PointCloud(points, copy=False)
+    target = PointCloud(points, copy=True)
+
+    man = LandmarkManager(target)
+    man['test_set'] = pcloud
+    man_copy = man.copy()
+
+    assert (man_copy._target is man._target)
+    assert (man_copy['test_set'] is not man['test_set'])
+    assert (not is_same_array(man_copy['test_set'].lms.points,
+                              man['test_set'].lms.points))
