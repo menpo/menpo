0c215232000761dbf4da0ec050d6914753b922cf
Epameinondas Antonakos <antonakosn@gmail.com>
2014-05-17 18:03:05 +0100

    adds clm_temp

--- menpo/benchmark/predefined.py
+++ menpo/benchmark/predefined.py
@@ -184,7 +184,7 @@ def clm_basic(training_db_path, training_db_ext, fitting_db_path,
                                  "function/closure or None")
 
     # predefined options
-    db_loading_options = {'crop_proportion': 0.3,
+    db_loading_options = {'crop_proportion': 0.4,
                           'convert_to_grey': True
                           }
     training_options = {'group': 'PTS',
@@ -194,8 +194,8 @@ def clm_basic(training_db_path, training_db_ext, fitting_db_path,
                         'normalization_diagonal': None,
                         'n_levels': 3,
                         'downscale': 1.1,
-                        'scaled_shape_models': True,
-                        'pyramid_on_features': True,
+                        'scaled_shape_models': False,
+                        'pyramid_on_features': False,
                         'max_shape_components': None,
                         'boundary': 3,
                         'interpolator': 'scipy'
@@ -212,6 +212,92 @@ def clm_basic(training_db_path, training_db_ext, fitting_db_path,
 
     # set passed parameters
     training_options['feature_type'] = feature_type
+    training_options['classifier_type'] = classifier_type
+    initialization_options['noise_std'] = noise_std
+
+    # run experiment
+    training_images = load_database(training_db_path, training_db_ext,
+                                    db_loading_options=db_loading_options,
+                                    verbose=verbose)
+    clm = clm_build_benchmark(training_images,
+                              training_options=training_options,
+                              verbose=verbose)
+    fitting_images = load_database(fitting_db_path, fitting_db_ext,
+                                   db_loading_options=db_loading_options,
+                                   verbose=verbose)
+    fitting_results = clm_fit_benchmark(fitting_images, clm,
+                                        initialization_options=
+                                        initialization_options,
+                                        fitting_options=fitting_options,
+                                        verbose=verbose)
+
+    # convert results
+    max_error_bin = 0.05
+    bins_error_step = 0.005
+    final_error_curve, initial_error_curve, error_bins = \
+        convert_fitting_results_to_ced(fitting_results,
+                                       max_error_bin=max_error_bin,
+                                       bins_error_step=bins_error_step)
+
+    # plot results
+    if plot:
+        title = "CLMs with {} and {} classifier using RLMS".format(
+            training_options['feature_type'],
+            training_options['classifier_type'])
+        y_axis = [final_error_curve, initial_error_curve]
+        legend = ['Fitting', 'Initialization']
+        plot_fitting_curves(error_bins, y_axis, title, new_figure=True,
+                            x_limit=max_error_bin, legend=legend,
+                            color_list=['r', 'b'], marker_list=['o', 'x'])
+    return fitting_results, final_error_curve, initial_error_curve, error_bins
+
+
+def clm_temp(training_db_path, training_db_ext, fitting_db_path,
+             fitting_db_ext, feature_type=sparse_hog, patch_size=(5, 5),
+             classifier_type=linear_svm_lr, scaled_shape_models=True,
+             pyramid_on_features=True, n_shape=None,
+             noise_std=0.04, verbose=False,
+             plot=False):
+    # check feature
+    if not isinstance(feature_type, str):
+        if not hasattr(feature_type, '__call__'):
+            if feature_type is not None:
+                raise ValueError("feature_type must be a string or "
+                                 "function/closure or None")
+
+    # predefined options
+    db_loading_options = {'crop_proportion': 0.4,
+                          'convert_to_grey': True
+                          }
+    training_options = {'group': 'PTS',
+                        'classifier_type': linear_svm_lr,
+                        'patch_shape': (5, 5),
+                        'feature_type': sparse_hog,
+                        'normalization_diagonal': None,
+                        'n_levels': 3,
+                        'downscale': 1.1,
+                        'scaled_shape_models': False,
+                        'pyramid_on_features': False,
+                        'max_shape_components': None,
+                        'boundary': 3,
+                        'interpolator': 'scipy'
+                        }
+    fitting_options = {'algorithm': RegularizedLandmarkMeanShift,
+                       'pdm_transform': OrthoPDM,
+                       'global_transform': AlignmentSimilarity,
+                       'n_shape': [6, 6, 6],
+                       'max_iters': 50,
+                       'error_type': 'me_norm'
+                       }
+    initialization_options = {'noise_std': 0.01,
+                              'rotation': False}
+
+    # set passed parameters
+    training_options['feature_type'] = feature_type
+    training_options['patch_size'] = patch_size
+    training_options['scaled_shape_models'] = scaled_shape_models
+    training_options['pyramid_on_features'] = pyramid_on_features
+    fitting_options['n_shape'] = n_shape
     training_options['classifier_type'] = classifier_type
     initialization_options['noise_std'] = noise_std
 
