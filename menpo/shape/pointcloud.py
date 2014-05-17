0984c4d6cc47b372868f6e620330c12de514076e
Patrick Snape <patricksnape@gmail.com>
2014-05-17 08:51:27 +0100

    Add the trimesh copy function

--- menpo/shape/pointcloud.py
+++ menpo/shape/pointcloud.py
@@ -43,14 +43,14 @@ class PointCloud(Shape):
             self.points = np.array(points, copy=True, order='C')
 
     def copy(self):
-        r"""An efficient copy of this PointCloud.
+        r"""
+        An efficient copy of this PointCloud.
 
         Only landmarks and points will be transferred. For a full copy consider
-        using deepcopy().
+        using `deepcopy()`.
 
         Returns
         -------
-
         pointcloud: :map:`PointCloud`
             A PointCloud with the same points and landmarks as this one.
 
