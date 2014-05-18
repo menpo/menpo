1d67644eed7d01d831f4ca40b6df60d0ec5eb764
James Booth <jabooth@gmail.com>
2014-05-18 19:22:37 +0000

    Adds ability to ingest bounding box files

--- menpo/benchmark/__init__.py
+++ menpo/benchmark/__init__.py
@@ -2,3 +2,4 @@ from .predefined import (aam_fastest_alternating_noise,
                          aam_best_performance_alternating_noise,
                          aam_params_combinations_noise,
                          clm_params_combinations_noise, clm_basic_noise)
+from .io import download_ibug_bounding_boxes
