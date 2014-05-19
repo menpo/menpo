748fab46c76685407d1d4bd9f0808d077742f7dd
Epameinondas Antonakos <antonakosn@gmail.com>
2014-05-19 15:48:07 +0100

    fixes aam_builder_test

--- menpo/fitmultilevel/aam/test/aam_builder_test.py
+++ menpo/fitmultilevel/aam/test/aam_builder_test.py
@@ -1,4 +1,3 @@
-from unittest import skip
 import numpy as np
 from numpy.testing import assert_allclose
 from nose.tools import raises
@@ -10,9 +9,9 @@ from menpo.fitmultilevel.aam import AAMBuilder, PatchBasedAAMBuilder
 from menpo.fitmultilevel.featurefunctions import sparse_hog
 
 # load images
-filenames = ['breakingbad.jpg', 'lenna.png']
+filenames = ['breakingbad.jpg', 'takeo.ppm', 'lenna.png', 'einstein.jpg']
 training_images = []
-for i in range(2):
+for i in range(4):
     im = mio.import_builtin_asset(filenames[i])
     im.crop_to_landmarks_proportion(0.1)
     labeller(im, 'PTS', ibug_68_trimesh)
@@ -44,7 +43,7 @@ aam2 = AAMBuilder(feature_type=None,
                   scaled_shape_models=True,
                   pyramid_on_features=False,
                   max_shape_components=None,
-                  max_appearance_components=10,
+                  max_appearance_components=1,
                   boundary=0,
                   interpolator='scipy').build(training_images, group='PTS')
 
@@ -56,7 +55,7 @@ aam3 = AAMBuilder(feature_type='igo',
                   downscale=3,
                   scaled_shape_models=True,
                   pyramid_on_features=True,
-                  max_shape_components=[1],
+                  max_shape_components=[2],
                   max_appearance_components=10,
                   boundary=2,
                   interpolator='scipy').build(training_images, group='PTS')
@@ -107,7 +106,7 @@ def test_downscale_exception():
 def test_normalization_diagonal_exception():
     aam = AAMBuilder(normalization_diagonal=100).build(training_images,
                                                        group='PTS')
-    assert (aam.appearance_models[0].n_features == 378)
+    assert (aam.appearance_models[0].n_features == 410)
     aam = AAMBuilder(normalization_diagonal=10).build(training_images,
                                                       group='PTS')
 
@@ -130,76 +129,76 @@ def test_boundary_exception():
 
 
 def test_aam_1():
-    assert (aam1.n_training_images == 2)
+    assert (aam1.n_training_images == 4)
     assert (aam1.n_levels == 3)
     assert (aam1.downscale == 2)
     assert (aam1.feature_type[0] == 'igo' and aam1.feature_type[2] is None)
     assert (aam1.interpolator == 'scipy')
-    assert_allclose(np.around(aam1.reference_shape.range()), (110., 102.))
+    assert_allclose(np.around(aam1.reference_shape.range()), (109., 103.))
     assert (not aam1.scaled_shape_models)
     assert (not aam1.pyramid_on_features)
-    assert (np.all([aam1.shape_models[j].n_components == 1
-                    for j in range(aam1.n_levels)]))
-    assert (np.all([aam1.appearance_models[j].n_components == 1
+    assert_allclose([aam1.shape_models[j].n_components
+                     for j in range(aam1.n_levels)], (1, 2, 3))
+    assert (np.all([aam1.appearance_models[j].n_components == 3
                     for j in range(aam1.n_levels)]))
     assert_allclose([aam1.appearance_models[j].template_instance.n_channels
                      for j in range(aam1.n_levels)], (2, 36, 1))
     assert_allclose([aam1.appearance_models[j].components.shape[1]
-                     for j in range(aam1.n_levels)], (13892, 250056, 6946))
+                     for j in range(aam1.n_levels)], (14892, 268056, 7446))
 
 
 def test_aam_2():
-    assert (aam2.n_training_images == 2)
+    assert (aam2.n_training_images == 4)
     assert (aam2.n_levels == 2)
     assert (aam2.downscale == 1.2)
     assert (aam2.feature_type[0] is None and aam2.feature_type[1] is None)
     assert (aam2.interpolator == 'scipy')
-    assert_allclose(np.around(aam2.reference_shape.range()), (224., 207.))
+    assert_allclose(np.around(aam2.reference_shape.range()), (169., 161.))
     assert aam2.scaled_shape_models
     assert (not aam2.pyramid_on_features)
-    assert (np.all([aam2.shape_models[j].n_components == 1
+    assert (np.all([aam2.shape_models[j].n_components == 3
                     for j in range(aam2.n_levels)]))
     assert (np.all([aam2.appearance_models[j].n_components == 1
                     for j in range(aam2.n_levels)]))
     assert (np.all([aam2.appearance_models[j].template_instance.n_channels == 1
                     for j in range(aam2.n_levels)]))
     assert_allclose([aam2.appearance_models[j].components.shape[1]
-                     for j in range(aam2.n_levels)], (20447, 29443))
+                     for j in range(aam2.n_levels)], (12851, 18518))
 
 
 def test_aam_3():
-    assert (aam3.n_training_images == 2)
+    assert (aam3.n_training_images == 4)
     assert (aam3.n_levels == 1)
     assert (aam3.downscale == 3)
-    assert (aam3.feature_type[0] is 'igo' and len(aam3.feature_type) == 1)
+    assert (aam3.feature_type[0] == 'igo' and len(aam3.feature_type) == 1)
     assert (aam3.interpolator == 'scipy')
-    assert_allclose(np.around(aam3.reference_shape.range()), (224., 207.))
+    assert_allclose(np.around(aam3.reference_shape.range()), (169., 161.))
     assert aam3.scaled_shape_models
     assert aam3.pyramid_on_features
-    assert (np.all([aam3.shape_models[j].n_components == 1
+    assert (np.all([aam3.shape_models[j].n_components == 2
                     for j in range(aam3.n_levels)]))
-    assert (np.all([aam3.appearance_models[j].n_components == 1
+    assert (np.all([aam3.appearance_models[j].n_components == 3
                     for j in range(aam3.n_levels)]))
     assert (np.all([aam3.appearance_models[j].template_instance.n_channels == 2
                     for j in range(aam3.n_levels)]))
     assert_allclose([aam3.appearance_models[j].components.shape[1]
-                     for j in range(aam3.n_levels)], 58886)
+                     for j in range(aam3.n_levels)], 37036)
 
 
 def test_aam_4():
-    assert (aam4.n_training_images == 2)
+    assert (aam4.n_training_images == 4)
     assert (aam4.n_levels == 2)
     assert (aam4.downscale == 1.2)
-    assert (aam4.feature_type[0] is 'lbp')
+    assert (aam4.feature_type[0] == 'lbp')
     assert (aam4.interpolator == 'scipy')
-    assert_allclose(np.around(aam4.reference_shape.range()), (147., 135.))
+    assert_allclose(np.around(aam4.reference_shape.range()), (145., 138.))
     assert aam4.scaled_shape_models
     assert aam4.pyramid_on_features
     assert (np.all([aam4.shape_models[j].n_components == 1
                     for j in range(aam4.n_levels)]))
-    assert (np.all([aam4.appearance_models[j].n_components == 1
+    assert (np.all([aam4.appearance_models[j].n_components == 3
                     for j in range(aam4.n_levels)]))
     assert (np.all([aam4.appearance_models[j].template_instance.n_channels == 4
                     for j in range(aam4.n_levels)]))
     assert_allclose([aam4.appearance_models[j].components.shape[1]
-                     for j in range(aam4.n_levels)], (22572, 25168))
+                     for j in range(aam4.n_levels)], (23648, 25988))
