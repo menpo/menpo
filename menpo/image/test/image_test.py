a744ac3f04d42164aa598d5dab16e223b18657ca
James Booth <jabooth@gmail.com>
2014-05-21 21:49:15 +0100

    fix Boolean order kwarg test

--- menpo/image/test/image_test.py
+++ menpo/image/test/image_test.py
@@ -226,7 +226,7 @@ def test_boolean_bounds_false():
     assert(np.all(max_b == np.array([5, 4])))
 
 
-@raises(ValueError)
+@raises(TypeError)
 def test_boolean_prevent_order_kwarg():
     mask = BooleanImage.blank((8, 8), fill=True)
     mask.warp_to_mask(mask, None, order=4)
