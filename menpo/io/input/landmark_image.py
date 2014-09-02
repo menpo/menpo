50c4f4e86f61af6390dfb73e64c001e4d5d4f3af
James Booth <jabooth@gmail.com>
2014-09-02 17:06:03 +0100

    remove the TXTImporter for landmarks

--- menpo/io/input/landmark_image.py
+++ menpo/io/input/landmark_image.py
@@ -69,37 +69,3 @@ class ImagePTSImporter(PTSImporter):
             Array with `ys` as the first axis: `[ys; xs]`
         """
         return np.hstack([ys, xs])
-
-
-class ImageTXTImporter(TXTImporter):
-    r"""
-    Implements the :meth:`_build_points` method for images. Here, `y` is the
-    first axis.
-
-    Parameters
-    ----------
-    filepath : string
-        Absolute filepath of the landmarks
-    """
-
-    def __init__(self, filepath):
-        super(ImageTXTImporter, self).__init__(filepath)
-
-    def _build_points(self, xs, ys):
-        """
-        For images, `axis 0 = ys` and `axis 1 = xs`. Therefore, return the
-        appropriate points array ordering.
-
-        Parameters
-        ----------
-        xs : (N,) ndarray
-            Row vector of `x` coordinates
-        ys : (N,) ndarray
-            Row vector of `y` coordinates
-
-        Returns
-        -------
-        points : (N, 2) ndarray
-            Array with `ys` as the first axis: `[ys; xs]`
-        """
-        return np.hstack([ys, xs])
