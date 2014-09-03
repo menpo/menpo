0ba79ea4a2a8b77eb94f5cb9ef15bdf000c53fb3
James Booth <jabooth@gmail.com>
2014-09-03 09:08:51 +0100

    refactor PWA. Faster

--- menpo/transform/piecewiseaffine/__init__.py
+++ menpo/transform/piecewiseaffine/__init__.py
@@ -1,2 +1,2 @@
-from .base import CachedPWA as PiecewiseAffine  # the default PWA is the C one.
+from .base import PythonPWA as PiecewiseAffine  # the default PWA is the C one.
 from .base import TriangleContainmentError
