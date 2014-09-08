09036b08afa2f1d5d1ce6560cb7201557464b2ed
Epameinondas Antonakos <antonakosn@gmail.com>
2014-09-08 02:31:22 +0100

    adds align method for figure_options

--- menpo/visualize/widgets/__init__.py
+++ menpo/visualize/widgets/__init__.py
@@ -1,3 +1,3 @@
 from .base import (browse_images, visualize_aam, browse_fitted_images,
                    browse_iter_images, plot_ced)
-from .helpers import figure_options
+from .helpers import figure_options, format_figure_options
