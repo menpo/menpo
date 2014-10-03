a34d8b61ea75a4bc80df1eb5dea46135ff31c62d
James Booth <jabooth@gmail.com>
2014-10-03 20:43:50 +0100

    spin out menpo3d

--- menpo/__init__.py
+++ menpo/__init__.py
@@ -1,6 +1,7 @@
 from .base import menpo_src_dir_path, Vectorizable, Targetable
 from .base import DP, DX, DL
 
+from . import feature
 from . import fit
 from . import fitmultilevel
 from . import image
@@ -8,12 +9,9 @@ from . import io
 from . import landmark
 from . import math
 from . import model
-from . import rasterize
 from . import shape
 from . import transform
 from . import visualize
-
-from . import feature
 
 from ._version import get_versions
 __version__ = get_versions()['version']
