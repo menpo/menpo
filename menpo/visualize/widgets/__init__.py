9aa863d600b36678982c67e6160e65ea3e53d64d
Epameinondas Antonakos <antonakosn@gmail.com>
2014-10-28 16:29:38 +0000

    removes fit related widgets

--- menpo/visualize/widgets/__init__.py
+++ menpo/visualize/widgets/__init__.py
@@ -1,3 +1,1 @@
-from .base import (visualize_images, visualize_shapes, visualize_shape_model,
-                   visualize_appearance_model, visualize_aam, visualize_atm,
-                   visualize_fitting_results, plot_ced)
+from .base import visualize_images, visualize_shapes
