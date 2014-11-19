ed88240e5b7d086551dae7f6e186cc70ed42a06e
Patrick Snape <patricksnape@gmail.com>
2014-11-19 09:14:17 +0000

    Remove the use of triplot
    Use the the PointGraph viewer instead of triplot because triplot
    has some weird behaviour where it resizes the image and looks
    terrible.

--- menpo/visualize/__init__.py
+++ menpo/visualize/__init__.py
@@ -1,7 +1,7 @@
 from .base import (
     Viewable, Menpo3dErrorMessage,
     PointCloudViewer, PointCloudViewer2d, PointGraphViewer, TriMeshViewer,
-    TriMeshViewer2d, LandmarkViewer, LandmarkViewer2d, ImageViewer2d,
+    LandmarkViewer, LandmarkViewer2d, ImageViewer2d,
     AlignmentViewer2d)
-from .text_utils import progress_bar_str, print_dynamic, print_bytes
+from .text_utils import progress_bar_str, print_dynamic
 from .widgets import visualize_images, visualize_shapes
