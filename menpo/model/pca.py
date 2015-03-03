57f47913b3e4f200f0946145f257f48cfdcc8962
Patrick Snape <patricksnape@gmail.com>
2015-03-03 13:37:48 +0000

    Fix trim components for PCA
    A view was being returned, now the copy (much like image crop)
    enforces that the data is collected.

--- menpo/model/pca.py
+++ menpo/model/pca.py
@@ -449,13 +449,16 @@ class PCAModel(MeanInstanceLinearModel):
         self.n_active_components = n_components
 
         if self.n_active_components < self.n_components:
-            # set self.n_components to n_components
-            self._components = self._components[:self.n_active_components]
+            # Just stored so that we can fit < 80 chars
+            nac = self.n_active_components
+            # set self.n_components to n_components. We have to copy to ensure
+            # that the data is actually removed, otherwise a view is returned
+            self._components = self._components[:nac].copy()
             # store the eigenvalues associated to the discarded components
             self._trimmed_eigenvalues = \
                 self._eigenvalues[self.n_active_components:]
             # make sure that the eigenvalues are trimmed too
-            self._eigenvalues = self._eigenvalues[:self.n_active_components]
+            self._eigenvalues = self._eigenvalues[:nac].copy()
 
     def distance_to_subspace(self, instance):
         """
