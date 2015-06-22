77c53c1de02d01bacc89fed1f2ee94b3bf9a4f36
Joan Alabort <ja310@imperial.ac.uk>
2015-06-22 18:04:24 +0100

    Update documentation and code based on feedback

--- menpo/shape/groupops.py
+++ menpo/shape/groupops.py
@@ -1,9 +1,14 @@
 from __future__ import division
+from menpo.shape import PointCloud
 
 
 def mean_pointcloud(pointclouds):
     r"""
     Compute the mean of a `list` of :map:`PointCloud` or subclass objects.
+    The list is assumed to be homogeneous i.e all elements of the list are
+    assumed to belong to the same point cloud subclass just as all elements
+    are also assumed to have the same number of points and represent
+    semantically equivalent point clouds.
 
     Parameters
     ----------
@@ -16,5 +21,8 @@ def mean_pointcloud(pointclouds):
     mean_pointcloud : :map:`PointCloud` or subclass
         The mean point cloud or subclass.
     """
-    return pointclouds[0].from_vector(
-        sum(pc.points for pc in pointclouds) / len(pointclouds))
+    # make a temporary PointCloud (with copy=False for low overhead)
+    tmp_pc = PointCloud(sum(pc.points for pc in pointclouds) /
+                        len(pointclouds), copy=False)
+    # use the type of the first element in the list to rebuild from the vector
+    return pointclouds[0].from_vector(tmp_pc.as_vector())
