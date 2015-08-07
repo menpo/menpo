a9e01ea13e63d02fa753e240da2ba9420d0e57b7
Epameinondas Antonakos <antonakosn@gmail.com>
2015-08-07 16:51:14 +0300

    defines insert_patches in cython

--- menpo/image/extract_patches.pyx
+++ menpo/image/extract_patches.pyx
@@ -145,3 +145,55 @@ cpdef extract_patches(IMAGE_TYPES[:, :, :] image,
             total_index += 1
 
     return patches
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef insert_patches(IMAGE_TYPES[:, :, :, :, :] patches,
+                     IMAGE_TYPES[:, :, :] image,
+                     CENTRE_TYPES[:, :] centres,
+                     Py_ssize_t[:, :] offset,
+                     Py_ssize_t offset_index):
+    cdef:
+        Py_ssize_t n_centres = centres.shape[0]
+        object extents_size = [n_centres, 2]
+
+        Py_ssize_t patch_shape0 = patches.shape[3]
+        Py_ssize_t patch_shape1 = patches.shape[4]
+        Py_ssize_t half_patch_shape0 = patch_shape0 / 2
+        Py_ssize_t half_patch_shape1 = patch_shape1 / 2
+        Py_ssize_t add_to_patch0 = patch_shape0 % 2
+        Py_ssize_t add_to_patch1 = patch_shape1 % 2
+        Py_ssize_t image_shape0 = image.shape[1]
+        Py_ssize_t image_shape1 = image.shape[2]
+        Py_ssize_t n_channels = image.shape[0]
+
+        Py_ssize_t total_index = 0, i = 0
+
+        # Although it is faster to use malloc in this case, the change in syntax
+        # and the mental overhead of handling freeing memory is not considered
+        # worth it for these buffers. From simple tests it seems you only begin
+        # to see a performance difference when you have
+        # n_augmented_centres >~ 5000
+        Py_ssize_t[:, :] augmented_centers = np.empty(extents_size,
+                                                      dtype=np.intp)
+        Py_ssize_t[:, :] ext_s_max = np.empty(extents_size, dtype=np.intp)
+        Py_ssize_t[:, :] ext_s_min = np.empty(extents_size, dtype=np.intp)
+        Py_ssize_t[:, :] ins_s_max = np.empty(extents_size, dtype=np.intp)
+        Py_ssize_t[:, :] ins_s_min = np.empty(extents_size, dtype=np.intp)
+
+    calc_augmented_centers(centres, offset, augmented_centers)
+    calc_slices(augmented_centers, image_shape0, image_shape1, patch_shape0,
+                patch_shape1, half_patch_shape0, half_patch_shape1,
+                add_to_patch0, add_to_patch1, ext_s_min, ext_s_max, ins_s_min,
+                ins_s_max)
+
+    for i in range(n_centres):
+        image[:,
+              ext_s_min[total_index, 0]:ext_s_max[total_index, 0],
+              ext_s_min[total_index, 1]:ext_s_max[total_index, 1]
+             ] = \
+                 patches[i, offset_index, :,
+                         ins_s_min[total_index, 0]:ins_s_max[total_index, 0],
+                         ins_s_min[total_index, 1]:ins_s_max[total_index, 1]]
+        total_index += 1
