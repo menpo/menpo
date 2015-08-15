a9483aab23604efe67f9e2d948207c4c5d764a00
Epameinondas Antonakos <antonakosn@gmail.com>
2015-08-15 17:49:53 +0300

    adds view_patches(_with_landmarks)

--- menpo/image/__init__.py
+++ menpo/image/__init__.py
@@ -1,4 +1,4 @@
 from .base import Image, ImageBoundaryError
 from .boolean import BooleanImage
 from .masked import MaskedImage, OutOfMaskSampleError
-from .visualize_patches import create_patches_image
+from .visualize_patches import view_patches, view_patches_with_landmarks
--- menpo/image/visualize_patches.py
+++ menpo/image/visualize_patches.py
@@ -49,8 +49,10 @@ def create_patches_image(patches, patch_centers, patches_indices=None,
     return patches_image
 
 
-def render_rectangles_around_patches(centers, patch_shape, axes=None, image_view=True,
-                                     line_colour='r', line_style='-', line_width=1):
+def render_rectangles_around_patches(centers, patch_shape, axes=None,
+                                     image_view=True, line_colour='r',
+                                     line_style='-', line_width=1,
+                                     interpolation='none'):
     import matplotlib.pyplot as plt
     from matplotlib.patches import Rectangle
 
@@ -74,11 +76,140 @@ def render_rectangles_around_patches(centers, patch_shape, axes=None, image_view
         xi = 0
         yi = 1
 
+    # Set correct offsets so that the rectangle is tight to the patch
+    if interpolation == 'none':
+        off_start = 0.5
+        off_end = 0.
+    else:
+        off_start = 1.
+        off_end = 0.5
+
     # Render rectangles
     for p in range(centers.shape[0]):
-        xc = np.intp(centers[p, xi] - half_patch_shape[xi]) - 0.5
-        yc = np.intp(centers[p, yi] - half_patch_shape[yi]) - 0.5
-        axes.add_patch(Rectangle((xc, yc), patch_shape[xi], patch_shape[yi],
+        xc = np.intp(centers[p, xi] - half_patch_shape[xi]) - off_start
+        yc = np.intp(centers[p, yi] - half_patch_shape[yi]) - off_start
+        axes.add_patch(Rectangle((xc, yc),
+                                 patch_shape[xi] + off_end,
+                                 patch_shape[yi] + off_end,
                                  fill=False, edgecolor=line_colour,
                                  linewidth=line_width,
                                  linestyle=line_style_dict[line_style]))
+
+
+def view_patches(patches, patch_centers, patches_indices=None,
+                 offset_index=None, figure_id=None, new_figure=False,
+                 channels=None, interpolation='none', cmap_name=None, alpha=1.,
+                 render_patches_bboxes=True, bboxes_line_colour='b',
+                 bboxes_line_style='-', bboxes_line_width=1, render_axes=False,
+                 axes_font_name='sans-serif',  axes_font_size=10,
+                 axes_font_style='normal', axes_font_weight='normal',
+                 axes_x_limits=None, axes_y_limits=None, figure_size=(10, 8)):
+    # Create patches image
+    patches_image = create_patches_image(patches, patch_centers,
+                                         patches_indices=patches_indices,
+                                         offset_index=offset_index)
+
+    # Render patches image
+    patches_image.view(figure_id=figure_id, new_figure=new_figure,
+                       channels=channels, interpolation=interpolation,
+                       cmap_name=cmap_name, alpha=alpha,
+                       render_axes=render_axes, axes_font_name=axes_font_name,
+                       axes_font_size=axes_font_size,
+                       axes_font_style=axes_font_style,
+                       axes_font_weight=axes_font_weight,
+                       axes_x_limits=axes_x_limits,
+                       axes_y_limits=axes_y_limits, figure_size=figure_size)
+
+    # Render rectangles around patches
+    if render_patches_bboxes:
+        patch_shape = [patches.shape[3], patches.shape[4]]
+        render_rectangles_around_patches(
+            patches_image.landmarks['selected_patch_centers'].lms.points,
+            patch_shape, image_view=True, line_colour=bboxes_line_colour,
+            line_style=bboxes_line_style, line_width=bboxes_line_width,
+            interpolation=interpolation)
+
+
+def view_patches_with_landmarks(patches, patch_centers, patches_indices=None,
+                                offset_index=None, figure_id=None,
+                                new_figure=False, channels=None,
+                                interpolation='none', cmap_name=None, alpha=1.,
+                                render_patches_bboxes=True,
+                                bboxes_line_colour='r', bboxes_line_style='-',
+                                bboxes_line_width=1, render_lines=True,
+                                line_colour=None, line_style='-', line_width=1,
+                                render_markers=True, marker_style='o',
+                                marker_size=20, marker_face_colour=None,
+                                marker_edge_colour=None, marker_edge_width=1.,
+                                render_numbering=False,
+                                numbers_horizontal_align='center',
+                                numbers_vertical_align='bottom',
+                                numbers_font_name='sans-serif',
+                                numbers_font_size=10,
+                                numbers_font_style='normal',
+                                numbers_font_weight='normal',
+                                numbers_font_colour='k', render_legend=False,
+                                legend_title='', legend_font_name='sans-serif',
+                                legend_font_style='normal', legend_font_size=10,
+                                legend_font_weight='normal',
+                                legend_marker_scale=None, legend_location=2,
+                                legend_bbox_to_anchor=(1.05, 1.),
+                                legend_border_axes_pad=None, legend_n_columns=1,
+                                legend_horizontal_spacing=None,
+                                legend_vertical_spacing=None,
+                                legend_border=True,
+                                legend_border_padding=None, legend_shadow=False,
+                                legend_rounded_corners=False, render_axes=False,
+                                axes_font_name='sans-serif', axes_font_size=10,
+                                axes_font_style='normal',
+                                axes_font_weight='normal', axes_x_limits=None,
+                                axes_y_limits=None, figure_size=(10, 8)):
+    # Create patches image
+    patches_image = create_patches_image(patches, patch_centers,
+                                         patches_indices=patches_indices,
+                                         offset_index=offset_index)
+
+    # Render patches image
+    patches_image.view_landmarks(
+        channels=channels, group='all_patch_centers', figure_id=figure_id,
+        new_figure=new_figure, interpolation=interpolation, cmap_name=cmap_name,
+        alpha=alpha, render_lines=render_lines, line_colour=line_colour,
+        line_style=line_style, line_width=line_width,
+        render_markers=render_markers, marker_style=marker_style,
+        marker_size=marker_size, marker_face_colour=marker_face_colour,
+        marker_edge_colour=marker_edge_colour,
+        marker_edge_width=marker_edge_width, render_numbering=render_numbering,
+        numbers_horizontal_align=numbers_horizontal_align,
+        numbers_vertical_align=numbers_vertical_align,
+        numbers_font_name=numbers_font_name,
+        numbers_font_size=numbers_font_size,
+        numbers_font_style=numbers_font_style,
+        numbers_font_weight=numbers_font_weight,
+        numbers_font_colour=numbers_font_colour, render_legend=render_legend,
+        legend_title=legend_title, legend_font_name=legend_font_name,
+        legend_font_style=legend_font_style, legend_font_size=legend_font_size,
+        legend_font_weight=legend_font_weight,
+        legend_marker_scale=legend_marker_scale,
+        legend_location=legend_location,
+        legend_bbox_to_anchor=legend_bbox_to_anchor,
+        legend_border_axes_pad=legend_border_axes_pad,
+        legend_n_columns=legend_n_columns,
+        legend_horizontal_spacing=legend_horizontal_spacing,
+        legend_vertical_spacing=legend_vertical_spacing,
+        legend_border=legend_border,
+        legend_border_padding=legend_border_padding,
+        legend_shadow=legend_shadow,
+        legend_rounded_corners=legend_rounded_corners, render_axes=render_axes,
+        axes_font_name=axes_font_name, axes_font_size=axes_font_size,
+        axes_font_style=axes_font_style, axes_font_weight=axes_font_weight,
+        axes_x_limits=axes_x_limits, axes_y_limits=axes_y_limits,
+        figure_size=figure_size)
+
+    # Render rectangles around patches
+    if render_patches_bboxes:
+        patch_shape = [patches.shape[3], patches.shape[4]]
+        render_rectangles_around_patches(
+            patches_image.landmarks['selected_patch_centers'].lms.points,
+            patch_shape, image_view=True, line_colour=bboxes_line_colour,
+            line_style=bboxes_line_style, line_width=bboxes_line_width,
+            interpolation=interpolation)
