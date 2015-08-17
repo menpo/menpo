02c00a070ce93d8d6d2e63bdd6501fdb39e47395
Epameinondas Antonakos <antonakosn@gmail.com>
2015-08-17 15:36:50 +0300

    adds _parse_command()

--- menpo/visualize/widgets/tools.py
+++ menpo/visualize/widgets/tools.py
@@ -5988,3 +5988,338 @@ class IGOOptionsWidget(ipywidgets.FlexBox):
 
         # add new function
         self.add_render_function(render_function)
+
+
+def _str_is_int(cmd):
+    r"""
+    Function that returns ``True`` if a given `str` is a positive or negative
+    integer.
+
+    Parameters
+    ----------
+    cmd : `str`
+        The command string.
+    """
+    return cmd.isdigit() or (cmd.startswith('-') and cmd[1:].isdigit())
+
+
+def _parse_command_with_comma(cmd, length):
+    r"""
+    Function that parses a command for slicing which contains at least one comma
+    (``,``). The function returns a `list` with the integer values that are
+    included in the command. It also ignores any redundant whitespaces that may
+    exist in the command. For example ::
+
+        _parse_command_with_comma([1, 2,-3 ], 10)
+
+    returns ::
+
+        '[1, 2, -3]'
+
+    Parameters
+    ----------
+    cmd : `str`
+        The command string.
+    length : `int`
+        The length of the variable that will get sliced.
+
+    Returns
+    -------
+    cmd_list : `list`
+        The list that can be used for slicing after interpreting and evaluating
+        the provided command.
+
+    Raises
+    ------
+    ValueError
+        Command cannot start or end with ','.
+    ValueError
+        Command cannot contain a pattern of the form ',,'.
+    ValueError
+        Command cannot contain numbers greater than {length}.
+    ValueError
+        Command must contain positive or negative integers.
+    """
+    if cmd.startswith(',') or cmd.endswith(','):
+        # if cmd starts or ends with ',', raise an error
+        raise ValueError("Command cannot start or end with ','.")
+    else:
+        # get the parts in between commas
+        tmp_cmd = cmd.split(',')
+        # for each part
+        final_cmd = []
+        for i in tmp_cmd:
+            if len(i) == 0:
+                # this means that there was the ',,' pattern
+                raise ValueError("Command cannot contain a pattern of the "
+                                 "form ',,'.")
+            elif _str_is_int(i):
+                # if it is a positive or negative integer convert it to int
+                n = int(i)
+                if n >= length:
+                    raise ValueError("Command cannot contain numbers greater "
+                                     "than {}.".format(length))
+                else:
+                    final_cmd.append(n)
+            else:
+                # else raise an error
+                raise ValueError("Command must contain positive or negative "
+                                 "integers.")
+        return final_cmd
+
+
+def _parse_command_with_one_colon(cmd, length):
+    r"""
+    Function that parses a command for slicing which contains exactly one colon
+    (``:``). The function returns a `list` with the integer indices, after
+    interpreting the slicing command. It also ignores any redundant whitespaces
+    that may exist in the command. For example ::
+
+        _parse_command_with_one_colon(:3, 10)
+
+    returns ::
+
+        '[0, 1, 2]'
+
+    Parameters
+    ----------
+    cmd : `str`
+        The command string.
+    length : `int`
+        The length of the variable that will get sliced.
+
+    Returns
+    -------
+    cmd_list : `list`
+        The list that can be used for slicing after interpreting and evaluating
+        the provided command.
+
+    Raises
+    ------
+    ValueError
+        Command cannot contain numbers greater than {length}.
+    ValueError
+        Command must contain positive or negative integers.
+    """
+    # this is necessary in order to return ranges with negative slices
+    tmp_list = range(length)
+
+    if cmd.startswith(':'):
+        # cmd has the form ":3" or ":"
+        if len(cmd) > 1:
+            # cmd has the form ":3"
+            i = cmd[1:]
+            if _str_is_int(i):
+                n = int(i)
+                if n > length:
+                    raise ValueError("Command cannot contain numbers greater "
+                                     "than {}.".format(length))
+                else:
+                    return tmp_list[:n]
+            else:
+                raise ValueError("Command must contain positive or negative "
+                                 "integers.")
+        else:
+            # cmd is ":"
+            return tmp_list
+    elif cmd.endswith(':'):
+        # cmd has the form "3:" or ":"
+        if len(cmd) > 1:
+            # cmd has the form "3:"
+            i = cmd[:-1]
+            if _str_is_int(i):
+                n = int(i)
+                if n >= length:
+                    raise ValueError("Command cannot contain numbers greater "
+                                     "than {}.".format(length))
+                else:
+                    return tmp_list[n:]
+            else:
+                raise ValueError("Command must contain positive or negative "
+                                 "integers.")
+        else:
+            # cmd is ":"
+            return tmp_list
+    else:
+        # cmd has the form "3:10"
+        # get the parts before and after colon
+        tmp_cmd = cmd.split(':')
+        start = tmp_cmd[0]
+        end = tmp_cmd[1]
+
+        if _str_is_int(start) and _str_is_int(end):
+            start = int(start)
+            end = int(end)
+            if start >= length or end > length:
+                raise ValueError("Command cannot contain numbers greater "
+                                 "than {}.".format(length))
+            else:
+                return tmp_list[start:end]
+        else:
+            raise ValueError("Command must contain positive or negative "
+                             "integers.")
+
+
+def _parse_command_with_two_colon(cmd, length):
+    r"""
+    Function that parses a command for slicing which contains exactly two colons
+    (``:``). The function returns a `list` with the integer indices, after
+    interpreting the slicing command. It also ignores any redundant whitespaces
+    that may exist in the command. For example ::
+
+        _parse_command_with_two_colon(::3, 10)
+
+    returns ::
+
+        '[0, 3, 6, 9]'
+
+    Parameters
+    ----------
+    cmd : `str`
+        The command string.
+    length : `int`
+        The length of the variable that will get sliced.
+
+    Returns
+    -------
+    cmd_list : `list`
+        The list that can be used for slicing after interpreting and evaluating
+        the provided command.
+
+    Raises
+    ------
+    ValueError
+        Command cannot contain numbers greater than {length}.
+    ValueError
+        Command must contain positive or negative integers.
+    """
+    # this is necessary in order to return ranges with negative slices
+    tmp_list = range(length)
+
+    if cmd.startswith('::'):
+        # cmd has the form "::3" or "::"
+        if len(cmd) > 2:
+            # cmd has the form "::3"
+            i = cmd[2:]
+            if _str_is_int(i):
+                n = int(i)
+                return tmp_list[::n]
+            else:
+                raise ValueError("Command must contain positive or negative "
+                                 "integers.")
+        else:
+            # cmd is "::"
+            return tmp_list
+    elif cmd.endswith('::'):
+        # cmd has the form "3::" or "::"
+        if len(cmd) > 2:
+            # cmd has the form "3::"
+            i = cmd[:-2]
+            if _str_is_int(i):
+                n = int(i)
+                if n >= length:
+                    raise ValueError("Command cannot contain numbers greater "
+                                     "than {}.".format(length))
+                else:
+                    return tmp_list[n::]
+            else:
+                raise ValueError("Command must contain positive or negative "
+                                 "integers.")
+        else:
+            # cmd is "::"
+            return tmp_list
+    else:
+        # cmd has the form "1:8:2"
+        # get the parts in between colons
+        tmp_cmd = cmd.split(':')
+
+        start = tmp_cmd[0]
+        end = tmp_cmd[1]
+        step = tmp_cmd[2]
+
+        if _str_is_int(start) and _str_is_int(end) and _str_is_int(step):
+            start = int(start)
+            end = int(end)
+            step = int(step)
+            if start >= length or end > length:
+                raise ValueError("Command cannot contain numbers greater "
+                                 "than {}.".format(length))
+            else:
+                return tmp_list[start:end:step]
+        else:
+            raise ValueError("Command must contain positive or negative "
+                             "integers.")
+
+
+def _parse_command(cmd, length):
+    r"""
+    Function that parses a command for slicing. It is able to recognize any
+    slicing pattern of Python and detect pattern errors. Some characteristic
+    examples are ":3", ":-2", "3:", "::3", "3::", "1:8", "1:8:2", "1, 5, -3",
+    "range(10)", "range("1, 10, 2)" etc. The function returns a `list` with the
+    integer indices, after interpreting the slicing command. It also ignores any
+    redundant whitespaces that may exist in the command.
+
+    Parameters
+    ----------
+    cmd : `str`
+        The command string.
+    length : `int`
+        The length of the variable that will get sliced.
+
+    Returns
+    -------
+    cmd_list : `list`
+        The list that can be used for slicing after interpreting and evaluating
+        the provided command.
+
+    Raises
+    ------
+    ValueError
+        Command cannot contain numbers greater than {length}.
+    ValueError
+        Command must contain positive or negative integers.
+    """
+    # remove all redundant spaces from cmd
+    cmd = cmd.replace(" ", "")
+
+    # remove all brackets from cmd
+    cmd = cmd.replace("[", "")
+    cmd = cmd.replace("]", "")
+
+    # cmd has the form of "range(1, 10, 2)" or "range(10)"
+    if cmd.startswith("range(") and cmd.endswith(")"):
+        cmd = cmd[6:-1]
+        if cmd.count(",") > 0:
+            cmd = cmd.replace(",", ":")
+        else:
+            cmd = "0:" + cmd
+
+    # get number of ':' and number of ','
+    n_colon = cmd.count(":")
+    n_comma = cmd.count(",")
+
+    if n_comma > 0 and n_colon == 0:
+        # parse cmd given that it contains only ','
+        return _parse_command_with_comma(cmd, length)
+    elif n_comma == 0 and n_colon > 0:
+        # parse cmd given that it contains only ':'
+        if n_colon == 1:
+            return _parse_command_with_one_colon(cmd, length)
+        elif n_colon == 2:
+            return _parse_command_with_two_colon(cmd, length)
+        else:
+            raise ValueError("Command contains more than two ':'.")
+    elif n_comma == 0 and n_colon == 0:
+        # cmd has the form of "10"
+        if _str_is_int(cmd):
+            n = int(cmd)
+            if n >= length:
+                raise ValueError("Command cannot contain numbers greater "
+                                 "than {}.".format(length))
+            else:
+                 return n
+    elif len(cmd) == 0:
+        return []
+    else:
+        raise ValueError("Wrong command.")
