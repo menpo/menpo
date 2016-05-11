638f594db0dce887e8bdc95bb527d4dfe0d36217
James Booth <jabooth@gmail.com>
2016-05-11 15:43:05 +0100

    expose pickle_path, org io imports more logically

--- menpo/io/__init__.py
+++ menpo/io/__init__.py
@@ -1,8 +1,9 @@
-from .input import (import_image, import_images, import_video, import_videos,
-                    import_landmark_file, import_landmark_files,
-                    import_pickle, import_pickles,
-                    import_builtin_asset,
-                    image_paths, landmark_file_paths, video_paths,
-                    data_path_to, data_dir_path, ls_builtin_assets)
-from .output import (export_image, export_landmark_file, export_pickle,
-                     export_video)
+from .input import (
+    import_image, import_images, image_paths,
+    import_video, import_videos, video_paths,
+    import_landmark_file, import_landmark_files, landmark_file_paths,
+    import_pickle, import_pickles, pickle_paths,
+    import_builtin_asset, data_dir_path, data_path_to, ls_builtin_assets
+)
+from .output import (export_image, export_video,
+                     export_landmark_file, export_pickle)
--- menpo/io/input/__init__.py
+++ menpo/io/input/__init__.py
@@ -1,6 +1,7 @@
-from .base import (import_image, import_images, import_video, import_videos,
-                   import_builtin_asset,
-                   import_landmark_file, import_landmark_files,
-                   data_path_to, data_dir_path, ls_builtin_assets,
-                   image_paths, landmark_file_paths, video_paths,
-                   import_pickle, import_pickles)
+from .base import (
+    import_image, import_images, image_paths,
+    import_video, import_videos, video_paths,
+    import_landmark_file, import_landmark_files, landmark_file_paths,
+    import_pickle, import_pickles, pickle_paths,
+    import_builtin_asset, data_dir_path, data_path_to, ls_builtin_assets
+)
