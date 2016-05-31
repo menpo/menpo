e360d74ef78cf13a18d2b8be6fd7517a35ce6631
Patrick Snape <patricksnape@gmail.com>
2016-05-31 12:26:36 +0100

    Skip rasterize tests on Travis

--- menpo/image/test/rasterize_test.py
+++ menpo/image/test/rasterize_test.py
@@ -1,4 +1,6 @@
+import os
 import numpy as np
+import unittest
 from numpy.testing import assert_allclose
 from menpo.shape import PointCloud, PointUndirectedGraph
 from menpo.image import Image
@@ -10,6 +12,7 @@ line = PointUndirectedGraph(np.array([[2, 4.5], [8, 4.5]]),
                             adjacency_matrix=np.array([[0, 1], [1, 0]]))
 
 
+@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_matplotlib_basic():
     im = Image.init_blank([11, 11], fill=0, n_channels=1)
     im.landmarks['test'] = centre
@@ -21,6 +24,7 @@ def test_rasterize_matplotlib_basic():
     assert_allclose(new_im.pixels[:, 5, 5], [255, 0, 0])
 
 
+@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_matplotlib_basic_line():
     im = Image.init_blank([11, 11], fill=0, n_channels=1)
     im.landmarks['test'] = line
@@ -34,6 +38,7 @@ def test_rasterize_matplotlib_basic_line():
     assert_allclose(new_im.pixels[2, 5:8, 5], [255, 255, 255])
 
 
+@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_pillow_basic():
     im = Image.init_blank([11, 11], fill=0, n_channels=3)
     im.landmarks['test'] = centre
@@ -45,6 +50,7 @@ def test_rasterize_pillow_basic():
     assert_allclose(new_im.pixels[0, 3:6, 3:6], 255)
 
 
+@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_pillow_basic_line():
     im = Image.init_blank([11, 11], fill=0, n_channels=3)
     im.landmarks['test'] = line
