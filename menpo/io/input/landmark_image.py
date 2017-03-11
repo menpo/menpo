454759c4ed49af763439be29c42130c4b974fd6d
Patrick Snape <patricksnape@gmail.com>
2017-03-11 16:37:10 +0100

    Small changes to update compatibility
    Use the partial_doc method and support None for extension
    normalization

--- menpo/io/input/landmark_image.py
+++ menpo/io/input/landmark_image.py
@@ -1,10 +1,8 @@
-from functools import partial
+from menpo.base import partial_doc
 
 from .landmark import asf_importer, pts_importer
 
 
-asf_image_importer = partial(asf_importer, image_origin=True)
-asf_image_importer.__doc__ = asf_importer.__doc__
+asf_image_importer = partial_doc(asf_importer, image_origin=True)
 
-pts_image_importer = partial(pts_importer, image_origin=True)
-pts_image_importer.__doc__ = pts_importer.__doc__
+pts_image_importer = partial_doc(pts_importer, image_origin=True)
