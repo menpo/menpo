87937010bae5c0261bd9881a3053576912868b58
Patrick Snape <patricksnape@gmail.com>
2017-12-02 21:14:06 +0100

    Remove the skip on Travis
    We can run headless by using Agg

--- menpo/image/test/test_rasterize.py
+++ menpo/image/test/test_rasterize.py
@@ -1,18 +1,15 @@
-import os
 import numpy as np
-import unittest
 from numpy.testing import assert_allclose
-from menpo.shape import PointCloud, PointUndirectedGraph
+
 from menpo.image import Image
 from menpo.image.rasterize import rasterize_landmarks_2d
-
+from menpo.shape import PointCloud, PointUndirectedGraph
 
 centre = PointCloud([[4.5, 4.5]])
 line = PointUndirectedGraph(np.array([[2, 4.5], [8, 4.5]]),
                             adjacency_matrix=np.array([[0, 1], [1, 0]]))
 
 
-# @unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_matplotlib_basic():
     im = Image.init_blank([11, 11], fill=0, n_channels=1)
     im.landmarks['test'] = centre
@@ -25,7 +22,6 @@ def test_rasterize_matplotlib_basic():
     assert_allclose(new_im.pixels[:, 5, 5], [255, 0, 0])
 
 
-@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_matplotlib_basic_line():
     im = Image.init_blank([11, 11], fill=0, n_channels=1)
     im.landmarks['test'] = line
@@ -40,7 +36,6 @@ def test_rasterize_matplotlib_basic_line():
     assert_allclose(new_im.pixels[2, 5:8, 5], [255, 255, 255])
 
 
-@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_pillow_basic():
     im = Image.init_blank([11, 11], fill=0, n_channels=3)
     im.landmarks['test'] = centre
@@ -53,7 +48,6 @@ def test_rasterize_pillow_basic():
     assert_allclose(new_im.pixels[0, 3:6, 3:6], 255)
 
 
-@unittest.skipIf(os.environ.get('TRAVIS'), 'Skipping due to lack of DISPLAY on Travis')
 def test_rasterize_pillow_basic_line():
     im = Image.init_blank([11, 11], fill=0, n_channels=3)
     im.landmarks['test'] = line
