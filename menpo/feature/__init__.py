4ee2c34f9313b58e9cdac8a3e9816a01ceb59aa8
Thanos <apapaion@gmail.com>
2019-11-18 17:50:50 +0000

    Going back to a previous version of feature/visualize to define glyph and support menpowidgets

--- menpo/feature/__init__.py
+++ menpo/feature/__init__.py
@@ -1,8 +1,10 @@
-from .base import ndfeature, imgfeature
 from .features import (gradient, hog, lbp, es, igo, no_op, gaussian_filter,
                        daisy, normalize, normalize_norm, normalize_std,
                        normalize_var, features_selection_widget)
 # Optional dependencies may return nothing.
 from .optional import *
+
 from .predefined import sparse_hog, double_igo
-from .visualize import sum_channels
+
+from .base import ndfeature, imgfeature
+from .visualize import glyph, sum_channels
