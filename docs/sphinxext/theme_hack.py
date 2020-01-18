94377b2cb6c25d851feb3c35f28cfea7f9ab5543
Patrick Snape <patricksnape@gmail.com>
2020-01-18 21:26:03 +0100

    Also format the other files in the repo with black
    Like in the docs folder and setup.py

--- docs/sphinxext/theme_hack.py
+++ docs/sphinxext/theme_hack.py
@@ -1,4 +1,4 @@
 def setup(app):
-    app.add_stylesheet('rtd_hack.css')
+    app.add_stylesheet("rtd_hack.css")
 
-    return {'parallel_read_safe': True, 'parallel_write_safe': True}
+    return {"parallel_read_safe": True, "parallel_write_safe": True}
--- setup.py
+++ setup.py
@@ -5,23 +5,20 @@ from setuptools import setup, find_packages
 import versioneer
 
 # Please see conda/meta.yaml for other binary dependencies
-install_requires = ['numpy>=1.14',
-                    'scipy>=1.0',
-                    'matplotlib>=3.0',
-                    'pillow>=4.0']
+install_requires = ["numpy>=1.14", "scipy>=1.0", "matplotlib>=3.0", "pillow>=4.0"]
 
 if sys.version_info.major == 2:
-    install_requires.append('pathlib==1.0')
+    install_requires.append("pathlib==1.0")
 
 setup(
-    name='menpo',
+    name="menpo",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='A Python toolkit for handling annotated data',
-    author='The Menpo Team',
-    author_email='hello@menpo.org',
+    description="A Python toolkit for handling annotated data",
+    author="The Menpo Team",
+    author_email="hello@menpo.org",
     packages=find_packages(),
     install_requires=install_requires,
-    package_data={'menpo': ['data/*']},
-    tests_require=['pytest>=5.0', 'pytest-mock>=1.0', 'mock>=3.0', 'black>=19.0']
+    package_data={"menpo": ["data/*"]},
+    tests_require=["pytest>=5.0", "pytest-mock>=1.0", "mock>=3.0", "black>=19.0"],
 )
